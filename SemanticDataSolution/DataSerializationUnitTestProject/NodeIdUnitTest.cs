92150b9448ccb760786df7a20795d995682a1cc0
Mariusz <mpostol@cas.eu>
2015-05-01 16:47:48 +0200

    Added DataSerializationUnitTestProject project

---
+++ SemanticDataSolution/DataSerializationUnitTestProject/NodeIdUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UAOOI.SemanticData.UANodeSetValidation.DataSerialization.UnitTest
+{
+  [TestClass]
+  public class NodeIdUnitTest
+  {
+
+    [TestMethod]
+    public void NodeIdValidTestMethod1()
+    {
+      //Numeric
+      NodeId _ni = NodeId.Parse("i=13");
+      Assert.IsNotNull(_ni);
+      _ni = NodeId.Parse("i=0");
+      Assert.IsNotNull(_ni);
+      //STRING
+      _ni = NodeId.Parse("ns=10;s=Hello:World");
+      Assert.IsNotNull(_ni);
+      //GUID
+      _ni = NodeId.Parse("g=09087e75-8e5e-499b-954f-f2a9603db28a");
+      Assert.IsNotNull(_ni);
+      //OPAQUE
+      _ni = NodeId.Parse("ns=1;b=M/RbKBsRVkePCePcx24oRA==");
+      Assert.IsNotNull(_ni);
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ServiceResultException))]
+    public void NodeIdNonValidNumericTestMethod1()
+    {
+      NodeId _ni;
+      _ni = NodeId.Parse("ns=10;i=-1"); //this example is in the specification.
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ServiceResultException))]
+    public void NodeIdNonValidNumericTestMethod2()
+    {
+      NodeId _ni;
+      _ni = NodeId.Parse("ns=-10;i=01"); //this example is in the specification.
+    }
+
+  }
+}

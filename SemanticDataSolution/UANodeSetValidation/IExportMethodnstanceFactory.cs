4e3b21a237b1bfe70d139008bf14e1012779318c
Mariusz <mpostol@cas.eu>
2015-05-09 23:07:02 +0200

    Porting NodeSet importer to the GitHub  two files have not been added.

---
+++ SemanticDataSolution/UANodeSetValidation/IExportMethodnstanceFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UAOOI.SemanticData.UANodeSetValidation
+{
+  public interface IExportMethodnstanceFactory : IExportInstanceFactory
+  {
+     bool NonExecutable
+    {
+      set;
+    }
+     bool NonExecutableSpecified
+    {
+      set;
+    }
+
+     IExportMethodnstanceFactory Clone();
+  }
+}
---
+++ SemanticDataSolution/UANodeSetValidation/IExportReferenceTypeFactory.cs
@@ -0,0 +1,21 @@
+﻿using UAOOI.SemanticData.UANodeSetValidation.XML;
+
+namespace UAOOI.SemanticData.UANodeSetValidation
+{
+  public interface IExportReferenceTypeFactory : IExportTypeFactory
+  {
+     LocalizedText[] InverseName
+    {
+      set;
+    }
+    /// <summary>
+    /// Sets a value indicating whether this <see cref="IExportReferenceTypeFactory"/> is symmetric.
+    /// </summary>
+    /// <remarks>Default Value is <b>false</b></remarks>
+    /// <value><c>true</c> if symmetric; otherwise, <c>false</c>.</value>
+     bool Symmetric
+    {
+      set;
+    }
+  }
+}

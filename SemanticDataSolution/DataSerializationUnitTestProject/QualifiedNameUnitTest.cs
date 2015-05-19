63a620fdb00d571062ef63fe89bec49f58dde27e
Mariusz <mpostol@cas.eu>
2015-05-19 22:57:59 +0200

    QualifiedName - add unit tests - development in progress

--- SemanticDataSolution/DataSerializationUnitTestProject/QualifiedNameUnitTest.cs
+++ SemanticDataSolution/DataSerializationUnitTestProject/QualifiedNameUnitTest.cs
@@ -10,14 +10,27 @@ namespace UAOOI.SemanticData.DataSerialization.UnitTest
     [TestMethod]
     public void QualifiedNameTestMethod1()
     {
-      QualifiedName _qn = new QualifiedName("Name");
+      string name = "Default Binary";
+      QualifiedName _qn = new QualifiedName("Default Binary");
       Assert.IsNotNull(_qn);
+      //Assert.AreEqual<int>(_qn.NamespaceIndex, 0);
+      Assert.IsFalse(_qn.NamespaceIndexSpecified);
+      Assert.AreEqual<string>(_qn.Name, name);
     }
     [TestMethod]
     public void QualifiedNameParseTestMethod2()
     {
       QualifiedName _qn = QualifiedName.Parse("0:Name");
       Assert.IsNotNull(_qn);
+    }
+    [TestMethod]
+    public void QualifiedNameParseTestMethod3()
+    {
+      QualifiedName _qn = QualifiedName.Parse("Name"); //Cannot find information that the NamespaceIndex is optional
+      Assert.IsNotNull(_qn);
+      Assert.AreEqual<int>(_qn.NamespaceIndex, 0);
+      Assert.IsFalse(_qn.NamespaceIndexSpecified);
+      Assert.AreEqual<string>(_qn.Name, "Name");
     }
   }
 }

69e9c5d091db336f1937ed96e2c61696c137640f
pguzek <guzekprzemyslaw@gmail.com>
2015-06-07 23:25:43 +0200

    Przemysław Guzek, Oskar Bogacz - ScructureEditor
    Zaimplementowany mechanizm refleksji właściwości obiektu.
    Utworzony TreeView dynamicznie wypełniany wynikiem refleksji przez
    mechanizm Bindingu.
    Edycja wartości podanych w widoku umożliwia modyfikacje reflektowanego
    obiektu przez Command.
    
    W celach testowych utworzono projekt WPF:
    UAUserInterfaceTypeEditorsTestProject.
    Jego zadaniem jest wyświetlanie okna graficznego korzystającego z
    zdefiniowanego
    w StructureEditor.xaml UserControl, oraz zreflektowanie testowego
    obiektu.

---
+++ SemanticDataSolution/OPCUAUserInterfaceTypeEditors/Model/Reflector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace UAOOI.SemanticData.TypeEditors
+{
+	public static class Reflector
+	{
+		private delegate void SetValue<T>(T value);
+
+		public static IEnumerable<Data> ReflectObjectProperties(object obj, int reflectionLevel)
+		{
+			LinkedList<Data> root = new LinkedList<Data>();
+			root.AddLast(new Data(obj, obj.GetType().GetTypeInfo().Name, obj, true));
+			root.Last.Value.Children = FindProperties(obj, reflectionLevel);
+			return root;
+		}
+
+		private static LinkedList<Data> FindProperties(object obj, int level)
+		{
+			if(level < 1 || obj == null)
+			{
+				return new LinkedList<Data>();
+			}
+
+			Type type = obj.GetType();
+			PropertyInfo[] propInfo = type.GetProperties();
+
+			LinkedList<Data> reflected = new LinkedList<Data>();
+			foreach(PropertyInfo prop in propInfo)
+			{
+				if(prop.GetIndexParameters().Length > 0)
+				{
+					Data temp = new Data(obj, prop.Name , "<indexed>", true);
+					reflected.AddLast(temp);
+				}
+				else
+				{
+					Data temp = new Data(obj, prop.Name, prop.GetValue(obj, null), !prop.CanWrite);
+					if(level > 0)
+					{
+						temp.Children = FindProperties(prop.GetValue(obj, null), level - 1);
+					}
+					reflected.AddLast(temp);
+				}
+			}
+
+			return reflected;
+		}
+	}
+}
---
+++ SemanticDataSolution/OPCUAUserInterfaceTypeEditors/StructureEditorViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UAOOI.SemanticData.TypeEditors
+{
+	public class StructureEditorViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		//-------------------------------------------------------------------------------
+
+		public static object ReflectedObject { get; set; }
+
+		//-------------------------------------------------------------------------------
+
+		public int ReflectionLevel { get; set; }
+
+		//-------------------------------------------------------------------------------
+
+		public IEnumerable<Data> TreeItemsSource { get; private set; }
+
+		//-------------------------------------------------------------------------------
+
+		public ICommand ReflectCommand { get; private set; }
+
+		//-------------------------------------------------------------------------------
+
+		public StructureEditorViewModel()
+		{
+			ReflectionLevel = 3;
+
+			ReflectCommand = new CommandWrapper(param =>
+			{
+				TreeItemsSource = Reflector.ReflectObjectProperties(ReflectedObject, ReflectionLevel);
+				PropertyChanged(this, new PropertyChangedEventArgs("TreeItemsSource"));
+
+			}, param => true);
+		}
+	}
+}

4c13874818831a01f6dfd0d536b83cd88c53e34d
Mariusz <mpostol@cas.eu>
2015-06-21 00:08:25 +0200

    Add test-cases defined by the OPCF and more - added schema and excel template.

---
+++ SemanticDataSolution/UANodeSetValidation/XML/OPCFCTTInformationModelling.Svcutil.cmd
@@ -0,0 +1,3 @@
+﻿
+rem https://msdn.microsoft.com/en-us/library/aa347733(v=vs.110).aspx
+Svcutil OPCFCTTInformationModelling.xsd /N:http://commsvr.com/UAOOI/SemanticData/UANodeSetValidation/OPCFCTTInformationModelling.xsd,UAOOI.SemanticData.UANodeSetValidation.XML /o:OPCFCTTInformationModelling.cs /dconly /s /serializer:DataContractSerializer

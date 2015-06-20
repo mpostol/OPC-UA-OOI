3161db084fecdae3ee7dbcfb18590a3e82f189cc
Mariusz <mpostol@cas.eu>
2015-06-20 11:27:13 +0200

    Porting UANodeSet importer to the GitHub - added Parameter, refactoring

--- SemanticDataSolution/InformationModelFactory/IModelFactory.cs
+++ SemanticDataSolution/InformationModelFactory/IModelFactory.cs
@@ -1,8 +1,17 @@
 ﻿
 namespace UAOOI.SemanticData.InformationModelFactory
 {
+
+  /// <summary>
+  /// Delegate LocalizedTextFactory - encapsualtes a method that must be used to create <see cref="LocalizedTextFactory"/>
+  /// </summary>
+  /// <param name="localeField">The locale field.</param>
+  /// <param name="valueField">The value field.</param>
   public delegate void LocalizedTextFactory(string localeField, string valueField);
-  
+
+  /// <summary>
+  /// Interface IModelFactory defines a generalized implementation specyfic specific representation of th OPC UA Information Model.
+  /// </summary>
   public interface IModelFactory : INodeContainer
   {
 

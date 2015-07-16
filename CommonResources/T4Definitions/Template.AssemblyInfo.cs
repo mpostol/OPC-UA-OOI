5b8a00ba36ec37bacf4bed51578dd8c3e4c678e0
Mariusz <mpostol@cas.eu>
2015-07-16 22:47:59 +0200

    #16  Add versions information to the projects  - added transform templates

---
+++ CommonResources/T4Definitions/Template.AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿//_______________________________________________________________
+//  Title   : Assembly info for: <Define Assembly Title>
+//  System  : Microsoft VisualStudio 2013 / C#
+//
+//  Copyright (c) 2015 CommServer
+//  +48 (42) 686 25 47
+//  commserver@cas.eu
+//  www.commsvr.com
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("<Define Assembly Title>")]
+[assembly: AssemblyDescription("DefaultProductName: <Define Assembly Title>")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("commsvr.com")]
+[assembly: AssemblyProduct("DefaultProductName")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 CommServer")]
+[assembly: AssemblyTrademark("OPC-UA-OOI")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("<GUID>")]
+[assembly: AssemblyVersion("0.00.00.*")]
+[assembly: AssemblyFileVersion("0.00.00")]
+

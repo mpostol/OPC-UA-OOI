ea91606c812af9c1623a60b26dd51209d12465a3
Mariusz <mpostol@cas.eu>
2015-07-17 17:26:58 +0200

    #16 updated the version info to the InformationModelFactory.

--- SemanticDataSolution/AddressSpaceComplianceTestTool/Properties/AssemblyInfo.cs
+++ SemanticDataSolution/AddressSpaceComplianceTestTool/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿//_______________________________________________________________
-//  Title   : Assembly info for: UAOOI.SemanticData.AddressSpaceTestTool
+//  Title   : Assembly info for: AddressSpaceTestTool
 //  System  : Microsoft VisualStudio 2013 / C#
 //
 //  Copyright (c) 2015 CommServer
@@ -12,17 +12,17 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UAOOI.SemanticData.AddressSpaceTestTool")]
+[assembly: AssemblyTitle("AddressSpaceTestTool")]
 [assembly: AssemblyDescription("Command Line OPC UA AS Compliance Test Tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("commsvr.com")]
-[assembly: AssemblyProduct("AddressSpaceTestTool")]
+[assembly: AssemblyProduct("OPC UA SemanticData")]
 [assembly: AssemblyCopyright("Copyright (c) 2015 CommServer")]
 [assembly: AssemblyTrademark("OPC-UA-OOI")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("157926b5-f6a5-4496-84c2-f5f9dae937ae")]
-[assembly: AssemblyVersion("0.01.00.*")]
-[assembly: AssemblyFileVersion("0.01.00")]
+[assembly: AssemblyVersion("1.00.01.*")]
+[assembly: AssemblyFileVersion("1.00.01")]
 [assembly: InternalsVisibleToAttribute("AddressSpaceComplianceTestToolUnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001005b97a0972ff6b1" +
                                                                                           "3a8a9ff9c09503aea0e5e2fe29cb2275a0c0942182f4c3431814b6bc9a556d9fe0d7e7823439c1" +
                                                                                           "ba28521f6318e4c936c4461604ef668e9686c2021571b093e1bfba071b373bc56a07a3afdc120c" +
--- SemanticDataSolution/InformationModelFactory/Properties/AssemblyInfo.cs
+++ SemanticDataSolution/InformationModelFactory/Properties/AssemblyInfo.cs
@@ -1,16 +1,26 @@
-﻿using System.Reflection;
+﻿//_______________________________________________________________
+//  Title   : Assembly info for: InformationModelFactory
+//  System  : Microsoft VisualStudio 2013 / C#
+//
+//  Copyright (c) 2015 CommServer
+//  +48 (42) 686 25 47
+//  commserver@cas.eu
+//  www.commsvr.com
+//_______________________________________________________________
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("InformationModelFactory")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OPC UA Information Model Factory Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CommServer.com")]
-[assembly: AssemblyProduct("InformationModelFactory")]
-[assembly: AssemblyCopyright("Copyright © CommServer.com 2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("commsvr.com")]
+[assembly: AssemblyProduct("OPC UA SemanticData")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 CommServer")]
+[assembly: AssemblyTrademark("OPC-UA-OOI")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b364bbfb-0608-4ed6-936c-7dca0b386305")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.00.01.*")]
+[assembly: AssemblyFileVersion("1.00.01")]

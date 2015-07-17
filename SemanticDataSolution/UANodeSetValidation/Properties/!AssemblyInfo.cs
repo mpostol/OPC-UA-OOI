319acb7e011c6d8db7e70059f299bc7d29eaef87
Mariusz <mpostol@cas.eu>
2015-07-17 17:42:08 +0200

    #16 updated the version info to the USNodeSetValidation

--- SemanticDataSolution/UANodeSetValidation/Properties/!AssemblyInfo.cs
+++ SemanticDataSolution/UANodeSetValidation/Properties/!AssemblyInfo.cs
@@ -1,32 +1,22 @@
-﻿//_______________________________________________________________
-//  Title   : Assembly info for: USNodeSetValidation
-//  System  : Microsoft VisualStudio 2013 / C#
-//
-//  Copyright (c) 2015 CommServer
-//  +48 (42) 686 25 47
-//  commserver@cas.eu
-//  www.commsvr.com
-//_______________________________________________________________
-
+﻿
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("USNodeSetValidation")]
-[assembly: AssemblyDescription("OPC UA NodeSet Validation library")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("commsvr.com")]
-[assembly: AssemblyProduct("OPC UA SemanticData")]
-[assembly: AssemblyCopyright("Copyright (c) 2015 CommServer")]
-[assembly: AssemblyTrademark("OPC-UA-OOI")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("USNodeSetValidation")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("00867667-6c38-4678-b801-8ae89b906614")]
-[assembly: AssemblyVersion("1.00.01.*")]
-[assembly: AssemblyFileVersion("1.00.01")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleToAttribute("UAOOI.SemanticDataUnitTest, PublicKey=00240000048000009400000006020000002400005253413100040000010001005b97a0972ff6b1" +
                                                                              "3a8a9ff9c09503aea0e5e2fe29cb2275a0c0942182f4c3431814b6bc9a556d9fe0d7e7823439c1" +
                                                                              "ba28521f6318e4c936c4461604ef668e9686c2021571b093e1bfba071b373bc56a07a3afdc120c" +
                                                                              "5313d39a935cda64b759f857ebb3db483641444a5347e1564f8ba6d4fad2f968d3caf9991a4fa6" +
                                                                              "aa019ebe")]
-

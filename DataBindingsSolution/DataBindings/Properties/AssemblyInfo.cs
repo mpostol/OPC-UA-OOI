0a0f4d924e5fb4d976ce53c7572364ed15b84194
Mariusz <mpostol@cas.eu>
2015-07-18 14:49:39 +0200

    #16 - cleanup DataBindings

--- DataBindingsSolution/DataBindings/Properties/AssemblyInfo.cs
+++ DataBindingsSolution/DataBindings/Properties/AssemblyInfo.cs
@@ -1,36 +1,27 @@
-﻿using System.Reflection;
+﻿//_______________________________________________________________
+//  Title   : Assembly info for: DataBindings
+//  System  : Microsoft VisualStudio 2013 / C#
+//
+//  Copyright (c) 2015 CommServer
+//  +48 (42) 686 25 47
+//  commserver@cas.eu
+//  www.commsvr.com
+//_______________________________________________________________
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DataBindings")]
+[assembly: AssemblyDescription("OPC UA DataBindings Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CAS")]
-[assembly: AssemblyProduct("ClassLibrary1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("commsvr.com")]
+[assembly: AssemblyProduct("OPC UA DataBindings")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 CommServer")]
+[assembly: AssemblyTrademark("OPC-UA-OOI")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bc6960f9-d096-49a0-a8d1-c5d194e54370")]
+[assembly: AssemblyVersion("1.00.01.*")]
+[assembly: AssemblyFileVersion("1.00.01")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- DataBindingsSolution/DataBindingsUnitTest/Properties/AssemblyInfo.cs
+++ DataBindingsSolution/DataBindingsUnitTest/Properties/AssemblyInfo.cs
@@ -1,36 +1,27 @@
-﻿using System.Reflection;
+﻿//_______________________________________________________________
+//  Title   : Assembly info for: DataBindingsUnitTest
+//  System  : Microsoft VisualStudio 2013 / C#
+//
+//  Copyright (c) 2015 CommServer
+//  +48 (42) 686 25 47
+//  commserver@cas.eu
+//  www.commsvr.com
+//_______________________________________________________________
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("DataBindingsUnitTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("OPC UA DataBindings UtitTests")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DataBindingsUnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("commsvr.com")]
+[assembly: AssemblyProduct("OPC UA DataBindings")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 CommServer")]
+[assembly: AssemblyTrademark("OPC-UA-OOI")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("73e0467b-131e-4a9f-8986-cb00f6c58f62")]
+[assembly: AssemblyVersion("1.00.01.*")]
+[assembly: AssemblyFileVersion("1.00.01")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

9f4e47e468535052270bebd099cd33fe1cbe4665
Mariusz <mpostol@cas.eu>
2015-09-10 22:16:58 +0200

    #33 reviewed IDataTypeFieldFactory

--- SemanticDataSolution/InformationModelFactory/IDataTypeFieldFactory.cs
+++ SemanticDataSolution/InformationModelFactory/IDataTypeFieldFactory.cs
@@ -4,7 +4,7 @@ using System.Xml;
 namespace UAOOI.SemanticData.InformationModelFactory
 {
   /// <summary>
-  /// Interface IDataTypeFieldFactory - This interface defines a representation of a field within a DataType.
+  /// Interface <c>IDataTypeFieldFactory</c> - This interface defines a representation of a field within a structural DataType.
   /// </summary>
   public interface IDataTypeFieldFactory
   {
@@ -19,13 +19,14 @@ namespace UAOOI.SemanticData.InformationModelFactory
     /// <summary>
     /// Adds the description for the field in multiple locales
     /// </summary>
-    /// <param name="localeField">The locale field.</param>
-    /// <param name="valueField">The value field.</param>
+    /// <param name="localeField">The locale field specified as a string that is composed of a language component and a country/region component as specified by RFC 3066.</param>
+    /// <param name="valueField">The localized text.</param>
     void AddDescription(string localeField, string valueField);
     /// <summary>
-    /// Sets the identifier.
+    /// Sets the identifier the value associated with the field.
     /// </summary>
     /// <value>The identifier.</value>
+    //TODO IDataTypeFieldFactory.Identifier is not defined by the schema #46
     int? Identifier
     {
       set;
@@ -46,12 +47,13 @@ namespace UAOOI.SemanticData.InformationModelFactory
     {
       set;
     }
-    /// <summary>The field is a structure with a layout specified by the <see cref="IDataTypeDefinitionFactory"/>. 
+    /// <summary>Creates new object of <see cref="IDataTypeDefinitionFactory"/> for anonymous definition of the DatType.
+    /// The field is a structure with a layout specified by the <see cref="IDataTypeDefinitionFactory"/>.
     /// This field is optional.
     /// This field allows designers to create nested structures without defining a new DataType Node for each structure.
     /// This field is not specified for subtypes of Enumeration.
     /// </summary>
-    /// <value>The definition.</value>
+    /// <value>A new instance of <see cref="IDataTypeDefinitionFactory"/> encapsulating the DatType definition.</value>
     IDataTypeDefinitionFactory NewDefinition();
     /// <summary>
     /// The value associated with the field. This field is only specified for subtypes of Enumeration.
@@ -64,8 +66,9 @@ namespace UAOOI.SemanticData.InformationModelFactory
     /// <summary>
     /// Sets the symbolic name of the field. A symbolic name for the field that can be used in auto-generated code. It should only be 
     /// specified if the Name cannot be used for this purpose. Only letters, digits or the underscore (‘_’) are permitted.
+    /// This value is not exposed in the OPC UA Address Space
     /// </summary>
-    /// <value>The name of the symbolic.</value>
+    /// <value>The symbolic name to be used by the tool.</value>
     string SymbolicName
     {
       set;

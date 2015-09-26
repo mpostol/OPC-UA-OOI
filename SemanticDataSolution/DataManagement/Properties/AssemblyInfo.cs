7000c3841977dcf02d1d91ba0f54fe44fc8b02e2
Mariusz <mpostol@cas.eu>
2015-09-26 21:47:55 +0200

    #53 - added project

---
+++ SemanticDataSolution/DataManagement/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SemanticData DataManagement")]
+[assembly: AssemblyDescription("Library supportiong management of Semantic Data")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UAOOI")]
+[assembly: AssemblyCopyright("Copyright commsvr.com ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7bab290f-9a23-4237-ac02-e095dd1b807c")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

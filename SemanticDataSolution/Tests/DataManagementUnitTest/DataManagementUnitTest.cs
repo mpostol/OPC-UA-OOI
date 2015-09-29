550933c5018c9b7330f1965db7303810f1f298b3
Mariusz <mpostol@cas.eu>
2015-09-29 13:44:24 +0200

    #60 - project created

---
+++ SemanticDataSolution/Tests/DataManagementUnitTest/DataManagementUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataManagementUnitTest
+{
+  [TestClass]
+  public class DataManagementUnitTest
+  {
+    [TestMethod]
+    public void DataManagementTestMethod()
+    {
+      Assert.Inconclusive();
+    }
+  }
+}
--- SemanticDataSolution/Tests/DataManagementUnitTest/Properties/AssemblyInfo.cs
+++ SemanticDataSolution/Tests/DataManagementUnitTest/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SemanticData DataManagement")]
-[assembly: AssemblyDescription("Library supporting management of Semantic Data")]
+[assembly: AssemblyTitle("DataManagementUnitTest")]
+[assembly: AssemblyDescription("Unit tests for the DataManagement")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UAOOI")]
-[assembly: AssemblyCopyright("Copyright commsvr.com ©  2015")]
+[assembly: AssemblyProduct("DataManagementUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("7bab290f-9a23-4237-ac02-e095dd1b807c")]
+[assembly: Guid("a1f0fb5c-7c17-4e1f-8ed0-cbbfb94f71ee")]
 [assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

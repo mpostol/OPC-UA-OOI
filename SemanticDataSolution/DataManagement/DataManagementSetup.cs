6cabaff277317ac2db0c2b6c2871262225444bd6
Mariusz <mpostol@cas.eu>
2015-10-06 14:07:49 +0200

    #66 - added the class and UT framework - development in progress.

---
+++ SemanticDataSolution/DataManagement/DataManagementSetup.cs
@@ -0,0 +1,33 @@
+﻿
+namespace UAOOI.SemanticData.DataManagement
+{
+  /// <summary>
+  /// Class DataManagementSetup -it is place holder to gather all external injection points used to initialize 
+  /// the communication and bind to local resources.
+  /// </summary>
+  public class DataManagementSetup
+  {
+
+    #region Injection points
+    public IBindingFactory BindingFactory { get; set; }
+    public IEncodingFactory EncodingFactory { get; set; }
+    public IMessageHandlerFactory MessageHandlerFactory { get; set; }
+    public IConfigurationFactory ConfigurationFactory { get; set; }
+    #endregion    
+    #region Internal control entry points
+    internal AssociationsCollection AssociationsCollection { get; private set; }
+    internal MessageHandlersCollection MessageHandlersCollection { get; private set; }
+    #endregion
+
+    #region Master Controll functioanlity 
+    public void Initialize()
+    {
+      throw new System.NotImplementedException();
+    }
+    public void Run()
+    {
+      throw new System.NotImplementedException();
+    }
+    #endregion
+  }
+}
---
+++ SemanticDataSolution/Tests/DataManagementUnitTest/DataManagementSetupUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UAOOI.SemanticData.DataManagement.UnitTest
+{
+  [TestClass]
+  public class DataManagementSetupUnitTest
+  {
+    [TestMethod]
+    [TestCategory("DataManagement_DataManagementSetup")]
+    public void DataManagementSetupCreatorTestMethod()
+    {
+      DataManagementSetup _ndm = new DataManagementSetup();
+      Assert.IsNotNull(_ndm);
+      Assert.IsNull(_ndm.BindingFactory);
+      Assert.IsNull(_ndm.ConfigurationFactory);
+      Assert.IsNull(_ndm.EncodingFactory);
+      Assert.IsNull(_ndm.MessageHandlerFactory);
+    }
+    [TestMethod]
+    [TestCategory("DataManagement_DataManagementSetup")]
+    [ExpectedException(typeof(NotImplementedException))]
+    public void InitializeTestMethod()
+    {
+      DataManagementSetup _ndm = new DataManagementSetup();
+      Assert.IsNotNull(_ndm);
+      _ndm.Initialize();
+    }
+    [TestMethod]
+    [TestCategory("DataManagement_DataManagementSetup")]
+    [ExpectedException(typeof(NotImplementedException))]
+    public void RunTestMethod()
+    {
+      DataManagementSetup _ndm = new DataManagementSetup();
+      Assert.IsNotNull(_ndm);
+      _ndm.Run();
+    }
+  }
+}

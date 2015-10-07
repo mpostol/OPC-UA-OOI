3e8821d37624a2146bc2827c00fc607e57ae461e
Mariusz <mpostol@cas.eu>
2015-10-07 13:21:12 +0200

    #67 - implementation of the collection done.

--- SemanticDataSolution/DataManagement/AssociationsCollection.cs
+++ SemanticDataSolution/DataManagement/AssociationsCollection.cs
@@ -5,8 +5,25 @@ using UAOOI.SemanticData.DataManagement.Configuration;
 
 namespace UAOOI.SemanticData.DataManagement
 {
+
+  /// <summary>
+  /// Class AssociationsCollection - a dictionary containing all <see cref="Association"/>
+  /// </summary>
+  /// <Note>
+  /// Current implementation does not support deletion of the <see cref="Association"/>. If the association is not longer needed call Disable on the <see cref="Association.State"/> object.
+  /// </Note>
   internal class AssociationsCollection : Dictionary<string, Association>
   {
+
+    #region interna API
+    /// <summary>
+    /// Creates the associations and populates the new dictionary with the associations instances created using the configuration <paramref name="configuration"/>.
+    /// </summary>
+    /// <param name="configuration">The configuration used to populate the collection.</param>
+    /// <param name="bindingFactory">The binding factory responsible to create and return <see cref="IBinding"/> instance for each association.</param>
+    /// <param name="encodingFactory">The encoding factory responsible to updated the created <see cref="IBinding"/> by provisioning all information necessary for encoding/decoding including <see cref="IValueConverter"/>.</param>
+    /// <returns>New dictionary of type <see cref="AssociationsCollection"/>.</returns>
+    /// <exception cref="System.ArgumentOutOfRangeException">Alias; Alias of any <see cref="Association"/> must be unique.</exception>
     internal static AssociationsCollection CreateAssociations(AssociationConfiguration[] configuration, IBindingFactory bindingFactory, IEncodingFactory encodingFactory)
     {
       AssociationsCollection _collection = new AssociationsCollection();
@@ -15,7 +32,7 @@ namespace UAOOI.SemanticData.DataManagement
       {
         if (_collection.ContainsKey(_ax.Alias))
           throw new ArgumentOutOfRangeException("Alias", "Alias of any Association must be unique");
-        SemanticData _newSemanticData = new SemanticData( new Uri(_ax.InformationModelURI), _ax.DataSymbolicName, null  );
+        SemanticData _newSemanticData = new SemanticData(new Uri(_ax.InformationModelURI), _ax.DataSymbolicName, null);
         switch (_ax.AssociationRole)
         {
           case AssociationRole.Consumer:
@@ -31,23 +48,56 @@ namespace UAOOI.SemanticData.DataManagement
       }
       return _collection;
     }
-    internal void AddMessageHandler(string alias, IMessageHandler messageHandler)
+    /// <summary>
+    /// Adds the message handler to the selected by the <paramref name="associationAlias" />.
+    /// </summary>
+    /// <remarks>
+    /// If this dictionary does not contain the <paramref name="associationAlias"/> key the request is skipped - no action is undertaken.
+    /// </remarks>
+    /// <param name="associationAlias">The association alias.</param>
+    /// <param name="messageHandler">The message handler to be associated.</param>
+    internal void AddMessageHandler(string associationAlias, IMessageHandler messageHandler)
     {
-      if (!this.ContainsKey(alias))
+      if (!this.ContainsKey(associationAlias))
         return;
-      Association _ass = this[alias];
+      Association _ass = this[associationAlias];
       _ass.AddMessageHandler(messageHandler);
     }
     /// <summary>
     /// Handles the configuration modifications.
     /// </summary>
-    /// <param name="sender">The sender.</param>
+    /// <param name="sender">The sender of the modification.</param>
     /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-    /// <exception cref="System.NotImplementedException"> It is intentionally not implemented - it is placeholder for further development in case there need be.</exception>
+    /// <remarks>
+    /// Limitation: It is intentionally not implemented - it is placeholder for further development in case there need be.
+    /// This handler could be called after recognition of of any modification over the out of bound communication, e.g. OPC UA Client/Server session or configuration file.
+    /// </remarks>
+    /// <exception cref="System.NotImplementedException">It is intentionally not implemented - it is placeholder for further development in case there need be.</exception>
     internal static void OnConfigurationChangeHandler(object sender, EventArgs e)
     {
-      throw new NotImplementedException("It is intentionally not implemented ");
+      throw new NotImplementedException("It is intentionally not implemented");
     }
+    /// <summary>
+    /// Initializes this instance - initializes and enables all the object in this collection. 
+    /// </summary>
+    internal void Initialize()
+    {
+      foreach (Association _ax in this.Values)
+      {
+        try
+        {
+          _ax.Initialize();
+          _ax.State.Enable();
+        }
+        catch (Exception)
+        {
+          //TODO add error handler to provide feedback to the user, e.g. message in the log file - intensionally not implemented. 
+        }
+      };
+    }
+    #endregion
+
+    #region private
     /// <summary>
     /// Class SemanticData - private implementation of the <see cref="ISemanticData"/>
     /// </summary>
@@ -62,22 +112,23 @@ namespace UAOOI.SemanticData.DataManagement
       }
       public Uri Identifier
       {
-        get; private set;
+        get;
+        private set;
       }
       public string SymbolicName
       {
-        get; private set;
+        get;
+        private set;
       }
       public IComparable NodeId
       {
-        get; private set;
+        get;
+        private set;
       }
     }
     private AssociationsCollection() { }
+    #endregion
 
-    internal void Initialize()
-    {
-      throw new NotImplementedException();
-    }
   }
+
 }

47f95e0ff553b08e65aaf5cd0d6b7af973433ebb
Mariusz <mpostol@cas.eu>
2015-10-12 14:06:02 +0200

    #80 - added classes and marked as not implemented.

---
+++ SemanticDataSolution/Tests/DataManagementUnitTest/OPCUAServerProducerSimulatorUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UAOOI.SemanticData.DataManagement.UnitTest
+{
+  [TestClass]
+  public class OPCUAServerProducerSimulatorUnitTest
+  {
+    [TestMethod]
+    [TestCategory("DataManagement_OPCUAServerProducerSimulator")]
+    [ExpectedException(typeof(NotImplementedException))]
+    public void CreatorTestMethod()
+    {
+      DataManagementSetup _consumer = Simulator.OPCUAServerProducerSimulator.CreateDevice(new MyMessageHandlerFactory());
+    }
+    private class MyMessageHandlerFactory : MessageHandling.IMessageHandlerFactory
+    {
+      public MessageHandling.IMessageReader GetIMessageReader(string name, System.Xml.XmlElement configuration)
+      {
+        throw new NotImplementedException();
+      }
+      public MessageHandling.IMessageWriter GetIMessageWriter(string name, System.Xml.XmlElement configuration)
+      {
+        throw new NotImplementedException();
+      }
+    }
+  }
+}
---
+++ SemanticDataSolution/Tests/DataManagementUnitTest/Simulator/OPCUAServerSimulator.cs
@@ -0,0 +1,15 @@
+﻿
+namespace UAOOI.SemanticData.DataManagement.UnitTest.Simulator
+{
+  /// <summary>
+  /// Class OPCUAServerSimulator - the class is to be used as a simulator of Address Space component.
+  /// It is used to simulate producer role on the server side.
+  /// </summary>
+  internal class OPCUAServerProducerSimulator
+  {
+    internal static DataManagementSetup CreateDevice(MessageHandling.IMessageHandlerFactory messageHandlerFactory)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}

685ce752984b47c9520bbe087784f88604186539
Mariusz <mpostol@cas.eu>
2015-10-17 15:37:30 +0200

    #84 implemented MessageWriterBase and added binary encoding for build-in types.

--- SemanticDataSolution/Tests/DataManagementUnitTest/BinaryMessageWriterUnitTest.cs
+++ SemanticDataSolution/Tests/DataManagementUnitTest/BinaryMessageWriterUnitTest.cs
@@ -2,12 +2,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UAOOI.SemanticData.DataManagement.MessageHandling;
 using UAOOI.SemanticData.DataManagement.DataRepository;
+using System.IO;
 
 namespace UAOOI.SemanticData.DataManagement.UnitTest
 {
+
   [TestClass]
   public class BinaryMessageWriterUnitTest
   {
+
+    #region TestMethod
     [TestMethod]
     [TestCategory("DataManagement_BinaryMessageWriter")]
     public void CreatorTestMethod1()
@@ -35,6 +39,7 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
     {
       TypesMessageWriter _bmw = new TypesMessageWriter();
       _bmw.AttachToNetwork();
+      Assert.IsTrue(_bmw.State.State == HandlerState.Operational);
       ProducerBinding _binding = new ProducerBinding();
       _binding.Value = new Nullable<float>();
       ((IMessageWriter)_bmw).Send(x => _binding, 1);
@@ -45,14 +50,61 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
     {
       TypesMessageWriter _bmw = new TypesMessageWriter();
       _bmw.AttachToNetwork();
+      Assert.IsTrue(_bmw.State.State == HandlerState.Operational);
       ProducerBinding _binding = new ProducerBinding();
       _binding.Value = String.Empty;
-      object[] _values = new object[] { (ulong)123, (uint)123, (ushort)123, "123", (float)123, (sbyte)123, (long)123, (int)123, (short)123, (double)123, (decimal)123, new DateTime(123, 10, 1), (byte)123, true, 'A', new byte[] { 1, 2, 3 } };
-      ((IMessageWriter)_bmw).Send((x) => { _binding.Value = _values[x]; return _binding; }, _values.Length);
+      int _sentItems = 0;
+      ((IMessageWriter)_bmw).Send((x) => { _binding.Value = m_TestValues[x]; _sentItems++; return _binding; }, m_TestValues.Length);
+      Assert.AreEqual(m_TestValues.Length, _sentItems);
     }
+    [TestMethod]
+    [TestCategory("DataManagement_BinaryMessageWriter")]
+    public void BinaryMessageWriterTestMethod()
+    {
+      BinaryMessageWriter _bmw = new BinaryMessageWriter();
+      Assert.AreEqual<int>(0, _bmw.m_NumberOfSentBytes);
+      Assert.AreEqual<int>(0, _bmw.m_NumberOfAttachToNetwork);
+      Assert.AreEqual<int>(0, _bmw.m_NumberOfSentMessages);
+      Assert.AreEqual<HandlerState>(HandlerState.Disabled, _bmw.State.State);
+      _bmw.AttachToNetwork();
+      Assert.AreEqual<HandlerState>(HandlerState.Operational, _bmw.State.State);
+      Assert.AreEqual<int>(1, _bmw.m_NumberOfAttachToNetwork);
+      Assert.AreEqual<int>(0, _bmw.m_NumberOfSentBytes);
+      Assert.AreEqual<int>(0, _bmw.m_NumberOfSentMessages);
+      ProducerBinding _binding = new ProducerBinding();
+      _binding.Value = String.Empty;
+      int _sentItems = 0;
+      ((IMessageWriter)_bmw).Send((x) => { _binding.Value = m_TestValues[x]; _sentItems++; return _binding; }, m_TestValues.Length);
+      Assert.AreEqual(m_TestValues.Length, _sentItems);
+      Assert.AreEqual<int>(1, _bmw.m_NumberOfAttachToNetwork);
+      Assert.AreEqual<int>(67, _bmw.m_NumberOfSentBytes);
+      Assert.AreEqual<int>(1, _bmw.m_NumberOfSentMessages);
+      byte[] _shouldBeInBuffer = new byte[]	
+      {
+          0x7b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     //UInt64
+          0x7b, 0x00, 0x00, 0x00,                             //UInt32
+          0x7b, 0x00,                                         //UInt16
+          0x03, 0x31, 0x32, 0x33,                             //string
+          0x00, 0x00, 0xf6, 0x42,                             //Float
+          0x7b,                                               //sbyte
+          0x7b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     //Int64
+          0x7b, 0x00, 0x00, 0x00,                             //Int32
+          0x7b, 0x00,                                         //Int16
+          0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x5e, 0x40,     //Double
+          0x7b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     //Int32
+          0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,     //DateTime
+          0x7b,                                               //Byte
+          0x01,                                               //boolean
+          0x41,                                               //Char
+          0x01, 0x02, 0x03,                                   //byte[]
+      };
+      byte[] _isInBuffer = _bmw.Buffer;
+      CollectionAssert.AreEqual(_bmw.Buffer, _shouldBeInBuffer);
+    }
+    #endregion
 
-    private class TestClass
-    { }
+    #region private
+    private class TestClass { }
     private class ProducerBinding : IProducerBinding
     {
 
@@ -99,149 +151,7 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
       #endregion
 
     }
-
-    /// <summary>
-    /// Class MessageWriterBase - helper class that provides basic implementation of the <see cref="IMessageWriter"/>
-    /// </summary>
-    public abstract class MessageWriterBase : IMessageWriter
-    {
-
-      #region IMessageWriter
-      /// <summary>
-      /// Sends the data described by a data set collection to remote destination.
-      /// </summary>
-      /// <param name="producerBinding">Encapsulates functionality used by the <see cref="IMessageWriter" /> to collect all the data (data set items) required to prepare new message and send it over the network.</param>
-      /// <param name="length">Number of items to be send used to calculate the length of the message.</param>
-      /// <exception cref="System.ArgumentOutOfRangeException">
-      /// Impossible to convert null value
-      /// or
-      /// </exception>
-      void IMessageWriter.Send(Func<int, IProducerBinding> producerBinding, int length)
-      {
-        if (State.State != HandlerState.Operational)
-          return;
-        CreateMessage(length);
-        for (int i = 0; i < length; i++)
-        {
-          IProducerBinding _pb = producerBinding(i);
-          object _value = _pb.GetFromRepository();
-          if (_value == null)
-            throw new ArgumentOutOfRangeException("Impossible to convert null value");
-          Type _type = _value.GetType();
-          if (_type == typeof(byte[]))
-            Write((byte[])_value, _pb.Parameter);
-          else if (!IsIConvertible(_value, _pb.Parameter))
-            throw new ArgumentOutOfRangeException(string.Format("Impossible to convert {0}", _value));
-        }
-        SendMessage();
-      }
-      /// <summary>
-      /// If implemented in derived class gets the the state machine for this instance.
-      /// </summary>
-      /// <value>An object of <see cref="IAssociationState" /> providing implementation of the state machine governing this instance behavior.</value>
-      public abstract IAssociationState State
-      {
-        get;
-        protected set;
-      }
-      /// <summary>
-      /// Attaches to network - initialize the underlying protocol stack and establish the connection with the broker is applicable.
-      /// </summary>
-      /// <remarks>Depending on the message transport layer type implementation of this function varies.</remarks>
-      public abstract void AttachToNetwork();
-      #endregion
-
-      #region private
-
-      #region Writer
-      protected abstract void SendMessage();
-      protected abstract void CreateMessage(int length);
-      protected abstract void WriteUInt64(ulong value, object parameter);
-      protected abstract void WriteUInt32(uint value, object parameter);
-      protected abstract void WriteUInt16(ushort value, object parameter);
-      protected abstract void WriteString(string value, object parameter);
-      protected abstract void WriteSingle(float value, object parameter);
-      protected abstract void WriteSByte(sbyte value, object parameter);
-      protected abstract void WriteInt64(long value, object parameter);
-      protected abstract void WriteInt32(int value, object parameter);
-      protected abstract void WriteInt16(short value, object parameter);
-      protected abstract void WriteDouble(double value, object parameter);
-      protected abstract void WriteDecimal(decimal value, object parameter);
-      protected abstract void WriteDateTime(DateTime dateTime, object parameter);
-      protected abstract void WriteByte(byte value, object parameter);
-      protected abstract void WriteBool(bool value, object parameter);
-      protected abstract void WriteChar(char value, object parameter);
-      protected abstract void Write(byte[] value, object parameter);
-      #endregion
-
-      private bool IsIConvertible(object value, object parameter)
-      {
-        IConvertible _cv = value as IConvertible;
-        if (_cv == null)
-          return false;
-        switch (_cv.GetTypeCode())
-        {
-          case TypeCode.Boolean:
-            WriteBool((Boolean)value, parameter);
-            break;
-          case TypeCode.Byte:
-            WriteByte((Byte)value, parameter);
-            break;
-          case TypeCode.Char:
-            WriteChar((Char)value, parameter);
-            break;
-          case TypeCode.DBNull:
-            throw new ArgumentOutOfRangeException("the value cannot be TypeCode.DBNull");
-          case TypeCode.DateTime:
-            WriteDateTime((DateTime)value, parameter);
-            break;
-          case TypeCode.Decimal:
-            WriteDecimal((Decimal)value, parameter);
-            break;
-          case TypeCode.Double:
-            WriteDouble((Double)value, parameter);
-            break;
-          case TypeCode.Empty:
-            throw new ArgumentOutOfRangeException("the value cannot be TypeCode.Empty");
-          case TypeCode.Int16:
-            WriteInt16((Int16)value, parameter);
-            break;
-          case TypeCode.Int32:
-            WriteInt32((Int32)value, parameter);
-            break;
-          case TypeCode.Int64:
-            WriteInt64((Int64)value, parameter);
-            break;
-          case TypeCode.Object:
-            return false;
-          case TypeCode.SByte:
-            WriteSByte((SByte)value, parameter);
-            break;
-          case TypeCode.Single:
-            WriteSingle((Single)value, parameter);
-            break;
-          case TypeCode.String:
-            WriteString((String)value, parameter);
-            break;
-          case TypeCode.UInt16:
-            WriteUInt16((UInt16)value, parameter);
-            break;
-          case TypeCode.UInt32:
-            WriteUInt32((UInt32)value, parameter);
-            break;
-          case TypeCode.UInt64:
-            WriteUInt64((UInt64)value, parameter);
-            break;
-          default:
-            return false;
-        }
-        return true;
-      }
-
-      #endregion
-
-    }
-    public class TypesMessageWriter : MessageWriterBase
+    private class TypesMessageWriter : MessageWriterBase
     {
 
       #region creator
@@ -330,61 +240,195 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
       {
         MassageCreated = true;
       }
+      protected override void SendMessage() { }
+      #endregion
+
+      #region test infrastructure
       internal bool MassageCreated = false;
       #endregion
 
-      private System.IO.BinaryWriter m_BinaryWriter;
-      /// <summary>
-      /// Class MyState.
-      /// </summary>
-      private class MyState : IAssociationState
+    }
+    private class BinaryMessageWriter : MessageWriterBase
+    {
+
+      #region creator
+      public BinaryMessageWriter()
       {
+        State = new MyState();
+      }
+      #endregion
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MyState"/> class.
-        /// </summary>
-        public MyState()
-        {
-          State = HandlerState.Disabled;
-        }
-        /// <summary>
-        /// Gets the current state <see cref="HandlerState" /> of the <see cref="Association" /> instance.
-        /// </summary>
-        /// <value>The state of <see cref="HandlerState" /> type.</value>
-        public HandlerState State
-        {
-          get;
-          private set;
-        }
-        /// <summary>
-        /// This method is used to enable a configured <see cref="Association" /> object. If a normal operation is possible, the state changes into <see cref="HandlerState.Operational" /> state.
-        /// In the case of an error situation, the state changes into <see cref="HandlerState.Error" />. The operation is rejected if the current <see cref="State" />  is not <see cref="HandlerState.Disabled" />.
-        /// </summary>
-        /// <exception cref="System.ArgumentException">Wrong state</exception>
-        public void Enable()
-        {
-          if (State != HandlerState.Disabled)
-            throw new ArgumentException("Wrong state");
-          State = HandlerState.Operational;
-        }
-        /// <summary>
-        /// This method is used to disable an already enabled <see cref="Association" /> object.
-        /// This method call shall be rejected if the current State is <see cref="HandlerState.Disabled" /> or <see cref="HandlerState.NoConfiguration" />.
-        /// </summary>
-        /// <exception cref="System.ArgumentException">Wrong state</exception>
-        public void Disable()
-        {
-          if (State != HandlerState.Operational)
-            throw new ArgumentException("Wrong state");
-          State = HandlerState.Disabled;
-        }
-
+      #region MessageWriterBase
+      public override IAssociationState State
+      {
+        get;
+        protected set;
+      }
+      public override void AttachToNetwork()
+      {
+        Assert.AreNotEqual<HandlerState>(HandlerState.Operational, State.State);
+        State.Enable();
+        m_NumberOfAttachToNetwork++;
       }
       protected override void SendMessage()
       {
+        Assert.IsNotNull(m_BinaryWriter);
+        m_BinaryWriter.Close();
+        SendMessage(m_Output.ToArray());
+        m_BinaryWriter.Dispose();
+        m_BinaryWriter = null;
       }
 
+      protected override void CreateMessage(int length)
+      {
+        m_Output = new MemoryStream();
+        m_BinaryWriter = new BinaryWriter(m_Output);
+      }
+      protected override void WriteUInt64(ulong value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteUInt32(uint value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteUInt16(ushort value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteString(string value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteSingle(float value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteSByte(sbyte value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteInt64(long value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteInt32(int value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteInt16(short value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteDouble(double value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteDecimal(decimal value, object parameter)
+      {
+        long _valueLong = Convert.ToInt64(value);
+        m_BinaryWriter.Write(_valueLong);
+      }
+      protected override void WriteDateTime(DateTime value, object parameter)
+      {
+        if (value.Kind == DateTimeKind.Local)
+          value = value.ToUniversalTime();
+        long _ticks = value.Ticks;
+        if (_ticks >= DateTime.MaxValue.Ticks)
+          _ticks = Int64.MaxValue;
+        else
+        {
+          _ticks -= s_TimeBase.Ticks;
+          if (_ticks <= 0)
+            _ticks = 0;
+        }
+        m_BinaryWriter.Write(_ticks);
+      }
+      protected override void WriteByte(byte value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteBool(bool value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void WriteChar(char value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      protected override void Write(byte[] value, object parameter)
+      {
+        m_BinaryWriter.Write(value);
+      }
+      #endregion
+
+      #region private
+      //vars
+      private static readonly DateTime s_TimeBase = new DateTime(1601, 1, 1); //base DateTime to calculate tiskc sent over wire.
+      private BinaryWriter m_BinaryWriter;
+      private MemoryStream m_Output;
+      //methods
+      protected virtual void SendMessage(byte[] buffer)
+      {
+        m_NumberOfSentMessages++;
+        m_NumberOfSentBytes += buffer.Length;
+      }
+      #endregion
+
+      #region tetst instrumentation
+      internal int m_NumberOfSentMessages = 0;
+      internal int m_NumberOfSentBytes = 0;
+      internal int m_NumberOfAttachToNetwork;
+      internal byte[] Buffer { get { return m_Output.ToArray(); } }
+      #endregion
 
     }
+    private class MyState : IAssociationState
+    {
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="MyState"/> class.
+      /// </summary>
+      public MyState()
+      {
+        State = HandlerState.Disabled;
+      }
+      /// <summary>
+      /// Gets the current state <see cref="HandlerState" /> of the <see cref="Association" /> instance.
+      /// </summary>
+      /// <value>The state of <see cref="HandlerState" /> type.</value>
+      public HandlerState State
+      {
+        get;
+        private set;
+      }
+      /// <summary>
+      /// This method is used to enable a configured <see cref="Association" /> object. If a normal operation is possible, the state changes into <see cref="HandlerState.Operational" /> state.
+      /// In the case of an error situation, the state changes into <see cref="HandlerState.Error" />. The operation is rejected if the current <see cref="State" />  is not <see cref="HandlerState.Disabled" />.
+      /// </summary>
+      /// <exception cref="System.ArgumentException">Wrong state</exception>
+      public void Enable()
+      {
+        if (State != HandlerState.Disabled)
+          throw new ArgumentException("Wrong state");
+        State = HandlerState.Operational;
+      }
+      /// <summary>
+      /// This method is used to disable an already enabled <see cref="Association" /> object.
+      /// This method call shall be rejected if the current State is <see cref="HandlerState.Disabled" /> or <see cref="HandlerState.NoConfiguration" />.
+      /// </summary>
+      /// <exception cref="System.ArgumentException">Wrong state</exception>
+      public void Disable()
+      {
+        if (State != HandlerState.Operational)
+          throw new ArgumentException("Wrong state");
+        State = HandlerState.Disabled;
+      }
+
+    }
+    private object[] m_TestValues = new object[] { (ulong)123, (uint)123, (ushort)123, "123", (float)123, (sbyte)123, (long)123, (int)123, (short)123, (double)123, (decimal)123, new DateTime(1601, 1, 1), (byte)123, true, 'A', new byte[] { 1, 2, 3 } };
+    #endregion
+
   }
+
 }

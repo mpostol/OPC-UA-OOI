c9a4b105d3d0f4f69b891abd0687cdc1ad0fb847
Mariusz <mpostol@cas.eu>
2015-10-17 21:41:27 +0200

    #88 added ReadMessageCompletedTestMethod and tested;

--- SemanticDataSolution/Tests/DataManagementUnitTest/MessageReaderTestClass.cs
+++ SemanticDataSolution/Tests/DataManagementUnitTest/MessageReaderTestClass.cs
@@ -10,31 +10,48 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
   public class MessageReaderTestClass
   {
     [TestMethod]
-    public void TestMethod1()
+    [TestCategory("DataManagement_MessageReader")]
+    public void CreatorTestMethod()
     {
-
+      TestMessageReaderBase _bmw = new TestMessageReaderBase();
+      Assert.IsNotNull(_bmw);
+      _bmw.AttachToNetwork();
+      Assert.IsTrue(_bmw.State.State == HandlerState.Operational);
+    }
+    [TestMethod]
+    [TestCategory("DataManagement_MessageWriter")]
+    public void ReadMessageCompletedTestMethod()
+    {
+      TestMessageReaderBase _reader = new TestMessageReaderBase();
+      _reader.AttachToNetwork();
+      object _sender = null;
+      MessageEventArg _message = null;
+      _reader.ReadMessageCompleted += (x, y) => { _sender = x; _message = y; };
+      Assert.IsNull(_sender);
+      Assert.IsNull(_message);
+      _reader.GetMessageTest();
+      Assert.IsNotNull(_sender);
+      Assert.IsNotNull(_message);
+      Assert.AreSame(_reader, _sender);
+      Assert.AreSame(_reader, _message.MessageContent);
     }
     public abstract class MessageReaderBase : IMessageReader, IPeriodicDataMessage
     {
 
       #region IMessageReader
-      public event EventHandler<MessageEventArg> ReadMessageCompleted;
-      public IAssociationState State
+      public abstract IAssociationState State
       {
-        get { throw new NotImplementedException(); }
+        get;
+        protected set;
       }
-      public void AttachToNetwork()
+      bool IPeriodicDataMessage.IAmDestination(ISemanticData dataId)
       {
         throw new NotImplementedException();
       }
+      public abstract void AttachToNetwork();
+      public event EventHandler<MessageEventArg> ReadMessageCompleted;
       #endregion
-      private void RaiseReadMessageCompleted()
-      {
-        EventHandler<MessageEventArg> _handler = ReadMessageCompleted;
-        if (_handler == null)
-          return;
-        ReadMessageCompleted(this, new MessageEventArg(this));
-      }
+
       #region IPeriodicDataMessage
       void IPeriodicDataMessage.UpdateMyValues(Func<int, IConsumerBinding> update, int length)
       {
@@ -133,16 +150,32 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
       protected abstract object ReadChar();
       protected abstract object ReadByte();
       protected abstract object ReadBoolean();
-      bool IPeriodicDataMessage.IAmDestination(ISemanticData dataId)
+      protected abstract ulong ContentFilter { get; set; }
+      protected void RaiseReadMessageCompleted()
       {
-        throw new NotImplementedException();
+        EventHandler<MessageEventArg> _handler = ReadMessageCompleted;
+        if (_handler == null)
+          return;
+        ReadMessageCompleted(this, new MessageEventArg(this));
       }
 
-
-      protected abstract ulong ContentFilter { get; set; }
+      #region test instrumentation
+      internal virtual void GetMessageTest()
+      {
+        Assert.IsNotNull(ReadMessageCompleted);
+        this.RaiseReadMessageCompleted();
+      }
+      #endregion
     }
     private class TestMessageReaderBase : MessageReaderBase
     {
+
+      #region creator
+      public TestMessageReaderBase()
+      {
+        State = new MyState();
+      }
+      #endregion
 
       #region MessageReaderBase
       protected override object ReadUInt64()
@@ -208,9 +241,68 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
           throw new NotImplementedException();
         }
       }
+      public override void AttachToNetwork()
+      {
+        Assert.AreNotEqual<HandlerState>(HandlerState.Operational, State.State);
+        State.Enable();
+        m_NumberOfAttachToNetwork++;
+      }
+      public override IAssociationState State
+      {
+        get;
+        protected set;
+      }
+      #endregion
+
+      #region private
+      private int m_NumberOfAttachToNetwork;
       #endregion
 
     }
+    private class MyState : IAssociationState
+    {
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="MyState"/> class.
+      /// </summary>
+      public MyState()
+      {
+        State = HandlerState.Disabled;
+      }
+      /// <summary>
+      /// Gets the current state <see cref="HandlerState" /> of the <see cref="Association" /> instance.
+      /// </summary>
+      /// <value>The state of <see cref="HandlerState" /> type.</value>
+      public HandlerState State
+      {
+        get;
+        private set;
+      }
+      /// <summary>
+      /// This method is used to enable a configured <see cref="Association" /> object. If a normal operation is possible, the state changes into <see cref="HandlerState.Operational" /> state.
+      /// In the case of an error situation, the state changes into <see cref="HandlerState.Error" />. The operation is rejected if the current <see cref="State" />  is not <see cref="HandlerState.Disabled" />.
+      /// </summary>
+      /// <exception cref="System.ArgumentException">Wrong state</exception>
+      public void Enable()
+      {
+        if (State != HandlerState.Disabled)
+          throw new ArgumentException("Wrong state");
+        State = HandlerState.Operational;
+      }
+      /// <summary>
+      /// This method is used to disable an already enabled <see cref="Association" /> object.
+      /// This method call shall be rejected if the current State is <see cref="HandlerState.Disabled" /> or <see cref="HandlerState.NoConfiguration" />.
+      /// </summary>
+      /// <exception cref="System.ArgumentException">Wrong state</exception>
+      public void Disable()
+      {
+        if (State != HandlerState.Operational)
+          throw new ArgumentException("Wrong state");
+        State = HandlerState.Disabled;
+      }
+
+    }
+
   }
 
 }

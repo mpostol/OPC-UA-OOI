86dc54b4d59e585235d1bcf5688dd57c1716fc3f
Mariusz <mpostol@cas.eu>
2015-10-24 17:59:48 +0200

    #100 - promoted, tested - done and my be closed UT all green

---
+++ SemanticDataSolution/DataManagement/MessageHandling/BinaryPackageDecoder.cs
@@ -0,0 +1,34 @@
+﻿namespace UAOOI.SemanticData.DataManagement.MessageHandling
+{
+  /// <summary>
+  /// Class BinaryPackageDecoder - OPC UA binary package decoder.
+  /// </summary>
+  public abstract class BinaryPackageDecoder : BinaryMessageDecoder
+  {
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BinaryPackageDecoder"/> class.
+    /// </summary>
+    public BinaryPackageDecoder()
+    {
+      Header = PackageHeader.GetConsumerPackageHeader(this);
+    }
+
+    /// <summary>
+    /// Gets or sets the header of the package.
+    /// </summary>
+    /// <value>The header <see cref="PackageHeader"/>.</value>
+    public PackageHeader Header { get; set; }
+
+    #region private
+    /// <summary>
+    /// Reads the package headers.
+    /// </summary>
+    protected void ReadPackageHeaders()
+    {
+      Header.Synchronize();
+    }
+    #endregion
+
+  }
+}
--- SemanticDataSolution/Tests/DataManagementUnitTest/MessageReaderTestClass.cs
+++ SemanticDataSolution/Tests/DataManagementUnitTest/MessageReaderTestClass.cs
@@ -351,34 +351,7 @@ namespace UAOOI.SemanticData.DataManagement.UnitTest
     #endregion
 
     #region To be promoted to the codebase
-    /// <summary>
-    /// Class BinaryPackageDecoder - OPC UA binary package decoder.
-    /// </summary>
-    public abstract class BinaryPackageDecoder : BinaryMessageDecoder
-    {
 
-      /// <summary>
-      /// Initializes a new instance of the <see cref="BinaryPackageDecoder"/> class.
-      /// </summary>
-      public BinaryPackageDecoder()
-      {
-        Header = PackageHeader.GetConsumerPackageHeader(this);
-      }
-
-      /// <summary>
-      /// Gets or sets the header of the package.
-      /// </summary>
-      /// <value>The header <see cref="PackageHeader"/>.</value>
-      public PackageHeader Header { get; set; }
-
-      #region private
-      protected void ReadPackageHeaders()
-      {
-        Header.Synchronize();
-      }
-      #endregion
-
-    }
     public class BinaryUDPPackageReader : BinaryPackageDecoder, IDisposable
     {
 

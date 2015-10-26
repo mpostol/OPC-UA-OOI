bae0f572d05a462531a20dcd5d8efae7762d3f8b
Mariusz <mpostol@cas.eu>
2015-10-26 17:04:00 +0100

    #103 - added the project - may be closed.

---
+++ SemanticDataSolution/UANetworkingReferenceApplication/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace UAOOI.SemanticData.UANetworking.ReferenceApplication
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+}
---
+++ SemanticDataSolution/UANetworkingReferenceApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UAOOI.UANetworkingReferenceApplication")]
+[assembly: AssemblyDescription("UAOOI SemanticData UA-Networking Reference Application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mpostol")]
+[assembly: AssemblyProduct("UAOOI")]
+[assembly: AssemblyCopyright("Copyright commsvr.com ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page, 
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page, 
+                                              // app, or any theme specific resource dictionaries)
+)]
+

aa936b2f1603b31445bfc56ca4b9dbb8a7014043
mpostol <mpostol@cas.eu>
2016-01-04 09:22:18 +0100

    #160 - changed version inforamtion - prerelease refactoring.

--- DataBindingsSolution/DataBindings/Properties/AssemblyInfo.cs
+++ DataBindingsSolution/DataBindings/Properties/AssemblyInfo.cs
@@ -22,6 +22,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bc6960f9-d096-49a0-a8d1-c5d194e54370")]
-[assembly: AssemblyVersion("1.00.03.*")]
-[assembly: AssemblyFileVersion("1.00.03")]
+[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyFileVersion("1.1.0")]
 

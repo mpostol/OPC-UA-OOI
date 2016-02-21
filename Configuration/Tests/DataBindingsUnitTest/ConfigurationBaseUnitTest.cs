bfbb51d7e09c89c9e543e9553d8a1b12f87430fe
Mariusz <mpostol@cas.eu>
2016-02-21 11:34:40 +0100

    #171 - improved unit test to test against possibility to plug in http://www.commsvr.com/Products/OPCUA/UAModelDesigner.aspx, tested.

--- Configuration/Tests/DataBindingsUnitTest/ConfigurationBaseUnitTest.cs
+++ Configuration/Tests/DataBindingsUnitTest/ConfigurationBaseUnitTest.cs
@@ -29,13 +29,15 @@ namespace UAOOI.Configuration.DataBindings.UnitTest
     {
       ConfigurationBaseDerivedTest _instance = new ConfigurationBaseDerivedTest();
       int _OnModifiedCalled = 0;
-      _instance.OnModified += (x, y) => _OnModifiedCalled++;
+      _instance.OnModified += (x, y) => { _OnModifiedCalled++; Assert.IsTrue(y.ConfigurationFileChanged); };
       _instance.CurrentConfiguration = new ConfigurationData();
       Assert.AreEqual<int>(1, _OnModifiedCalled);
       _instance.CurrentConfiguration = _instance.CurrentConfiguration;
       Assert.AreEqual<int>(1, _OnModifiedCalled);
       _instance.CurrentConfiguration = new ConfigurationData();
       Assert.AreEqual<int>(2, _OnModifiedCalled);
+      _instance.CreateDefaultConfiguration();
+      Assert.AreEqual<int>(3, _OnModifiedCalled);
     }
     [TestMethod]
     [TestCategory("DataBindings_ConfigurationBaseUnitTest")]
@@ -63,6 +65,11 @@ namespace UAOOI.Configuration.DataBindings.UnitTest
     #region private
     private class ConfigurationBaseDerivedTest : ConfigurationBase<ConfigurationData>
     {
+      /// <summary>
+      /// Gets the default name of the file.
+      /// </summary>
+      /// <value>The default name of the file.</value>
+      /// <exception cref="NotImplementedException"></exception>
       public override string DefaultFileName
       {
         get
@@ -70,27 +77,50 @@ namespace UAOOI.Configuration.DataBindings.UnitTest
           throw new NotImplementedException();
         }
       }
-
+      /// <summary>
+      /// Creates the default configuration.
+      /// </summary>
       public override void CreateDefaultConfiguration()
       {
-        throw new NotImplementedException();
+        CurrentConfiguration = new ConfigurationData(); 
       }
-
+      /// <summary>
+      /// Creates automatically the instance configurations on the best effort basis.
+      /// </summary>
+      /// <param name="descriptors">The descriptors of nodes.</param>
+      /// <param name="SkipOpeningConfigurationFile">if set to <c>true</c> skip opening configuration file.</param>
+      /// <param name="CancelWasPressed">if set to <c>true</c> cancel was pressed.</param>
+      /// <exception cref="NotImplementedException"></exception>
       public override void CreateInstanceConfigurations(INodeDescriptor[] descriptors, bool SkipOpeningConfigurationFile, out bool CancelWasPressed)
       {
         throw new NotImplementedException();
       }
-
+      /// <summary>
+      /// Gets the configuration editor - user interface to edit the plug-in configuration file.
+      /// </summary>
+      /// <returns>Represents a window or dialog box that makes up an application's user interface to be used to edit configuration file.</returns>
+      /// <exception cref="NotImplementedException"></exception>
       public override void EditConfiguration()
       {
         throw new NotImplementedException();
       }
-
+      /// <summary>
+      /// Gets the instance to be used by a user to configure the selected node.
+      /// </summary>
+      /// <param name="descriptor">Provides identifying description of the node to be configured.</param>
+      /// <returns>Returned object provides access to the instance node configuration edition functionality.</returns>
+      /// <exception cref="NotImplementedException"></exception>
       public override IInstanceConfiguration GetInstanceConfiguration(INodeDescriptor descriptor)
       {
         throw new NotImplementedException();
       }
-
+      /// <summary>
+      /// Saves the configuration file to a specified location.
+      /// </summary>
+      /// <param name="solutionFilePath">The solution file path.</param>
+      /// <param name="configurationFile">The configuration file.</param>
+      /// <exception cref="NotImplementedException"></exception>
+      /// <remarks><paramref name="solutionFilePath" /> is to be used to create relative file path to configuration files used by the plug-in.</remarks>
       public override void SaveConfiguration(string solutionFilePath, FileInfo configurationFile)
       {
         throw new NotImplementedException();

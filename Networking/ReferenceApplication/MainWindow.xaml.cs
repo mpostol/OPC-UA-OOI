cda470ca8d5c8fc370313741bd20f76be6893c91
Mariusz <mpostol@cas.eu>
2016-04-18 22:42:20 +0200

    #184 - added Exit menu item

--- Networking/ReferenceApplication/MainWindow.xaml.cs
+++ Networking/ReferenceApplication/MainWindow.xaml.cs
@@ -11,5 +11,13 @@ namespace UAOOI.Networking.ReferenceApplication
     {
       InitializeComponent();
     }
+    private void CommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+    {
+      this.Close();
+    }
+    private void CommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+    {
+      e.CanExecute = true;
+    }
   }
 }

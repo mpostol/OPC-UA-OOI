4eb59ccb7f36401bf7facffd2d78ceda2cfc478d
mariusz postol <mpostol@cas.eu>
2018-03-31 16:50:55 +0200

    UAOOI.Common.Infrastructure Add NuGet package fixes #246 - UAOOI.Common.Infrastructure added auto-generated AsseblyInfo

--- Common/Infrastructure/Properties/AssemblyInfo.cs
+++ Common/Infrastructure/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
 ﻿
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("UAOOI.Common.Infrastructure")]
 [assembly: AssemblyDescription("Common Infrastructure Management Functionality Aimed at Object Oriented Internet")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("commsvr.com")]
-[assembly: AssemblyProduct("Object Oriented Internet")]
-[assembly: AssemblyCopyright("Copyright MPostol © 2018")]
+[assembly: AssemblyProduct("OOI Common")]
+[assembly: AssemblyCopyright("Copyright MPostol (c) 2018")]
 [assembly: AssemblyTrademark("Object Oriented Internet")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8817a671-abb8-463a-a8a3-ddcfe781a6b6")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.3.*")]
+[assembly: AssemblyFileVersion("2.0.3")]
+

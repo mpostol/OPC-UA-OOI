8f9d45db0f35fecbf5c643e0d0c40fedc7fdefb2
mpostol <mpostol@cas.eu>
2018-06-18 15:52:40 +0200

    Port the project Networking.UDPMessageHandler to .NET Standard fixes #265

--- Networking/SemanticData/Properties/AssemblyInfo.cs
+++ Networking/SemanticData/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿//_______________________________________________________________
-//  Title   : Assembly info for: UAOOI.Networking.UDPMessageHandler
-//  System  : Microsoft VisualStudio 2017 / C#
+//  Title   : Assembly info for: Semantic Data Reactive Networking Library
+//  System  : Microsoft VisualStudio 2018 / C#
 //
 //  Copyright MPostol (c) 2018
 //  +48 608 619 899
@@ -12,20 +12,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UAOOI.Networking.UDPMessageHandler")]
-[assembly: AssemblyDescription("OOI Reactive Networking: UDP IMessageHandlerFactory Implementation")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("commsvr.com")]
-[assembly: AssemblyProduct("OOI Reactive Networking")]
-[assembly: AssemblyCopyright("Copyright MPostol (c) 2018")]
 [assembly: AssemblyTrademark("Object Oriented Internet")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("d2eb89f9-321e-46e4-ba79-679138b82b23")]
-[assembly: AssemblyVersion("3.0.5.*")]
-[assembly: AssemblyFileVersion("3.0.5")]
 [assembly: InternalsVisibleTo(
-  "UAOOI.Networking.UDPMessageHandler.UnitTest, PublicKey=" +
+  "UAOOI.Networking.SemanticData.UnitTest, PublicKey=" +
     "00240000048000009400000006020000002400005253413100040000010001005b97a0972ff6b13a" +
     "8a9ff9c09503aea0e5e2fe29cb2275a0c0942182f4c3431814b6bc9a556d9fe0d7e7823439c1ba28" +
     "521f6318e4c936c4461604ef668e9686c2021571b093e1bfba071b373bc56a07a3afdc120c5313d3" +

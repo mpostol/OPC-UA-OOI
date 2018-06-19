33417faa41fb8b5f49eab2d5a73d7fb5b88b534a
mpostol <mpostol@cas.eu>
2018-06-19 21:09:23 +0200

    Port SemanticData folder to .NET Standard #268 - Working copy - there may be compiler erros - UT tests have wron names and must be renamed as separate commit

--- SemanticData/InformationModelFactory/Properties/AssemblyInfo.cs
+++ SemanticData/InformationModelFactory/Properties/AssemblyInfo.cs
@@ -9,18 +9,8 @@
 //_______________________________________________________________
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UAOOI.SemanticData.InformationModelFactory")]
-[assembly: AssemblyDescription("OPC UA Information Model Factory Library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("commsvr.com")]
-[assembly: AssemblyProduct("OOI SemanticData")]
-[assembly: AssemblyCopyright("Copyright MPostol (c) 2018")]
 [assembly: AssemblyTrademark("Object Oriented Internet")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid("b364bbfb-0608-4ed6-936c-7dca0b386305")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0")]

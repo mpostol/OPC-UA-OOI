66dff2cedd2070628f399fd5689c490eeaad0dd4
mpostol <mpostol@cas.eu>
2018-06-19 09:18:45 +0200

    Port Configuration folder to .NET Standard #266
    - ported Configuration.Networking.Upgrade

--- Configuration/NetworkingUpgrade/Properties/AssemblyInfo.cs
+++ Configuration/NetworkingUpgrade/Properties/AssemblyInfo.cs
@@ -2,15 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UAOOI.Configuration.Networking.Upgrade")]
-[assembly: AssemblyDescription("Imports networking configuration helpers from obsolete versions")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("commsvr.com")]
-[assembly: AssemblyProduct("UAOOI")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("UAOOI")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("bebe131a-c931-4c2c-9f9e-ed0d0b75df26")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Networking/UDPMessageHandler/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿//_______________________________________________________________
+//  Title   : Assembly info for: UAOOI.Networking.UDPMessageHandler
+//  System  : Microsoft VisualStudio 2017 / C#
+//
+//  Copyright MPostol (c) 2018
+//  +48 608 619 899
+//  commserver@cas.eu
+//  https://github.com/mpostol/OPC-UA-OOI
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTrademark("Object Oriented Internet")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d2eb89f9-321e-46e4-ba79-679138b82b23")]
+[assembly: InternalsVisibleTo(
+  "UAOOI.Networking.UDPMessageHandler.UnitTest, PublicKey=" +
+    "00240000048000009400000006020000002400005253413100040000010001005b97a0972ff6b13a" +
+    "8a9ff9c09503aea0e5e2fe29cb2275a0c0942182f4c3431814b6bc9a556d9fe0d7e7823439c1ba28" +
+    "521f6318e4c936c4461604ef668e9686c2021571b093e1bfba071b373bc56a07a3afdc120c5313d3" +
+    "9a935cda64b759f857ebb3db483641444a5347e1564f8ba6d4fad2f968d3caf9991a4fa6aa019ebe"
+)]

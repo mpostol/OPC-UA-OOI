78a3d42c7945fc3d4b7b6d84372b65e8b9f332b2
mpostol <mpostol@cas.eu>
2018-07-22 00:15:13 +0200

    Integrate the `Networking.Simulator.Boiler` project with the `ReferenceApplication` #302
    - Testing in progress - added test of ISemanticDataSource
    - UT :ok_hand:

--- Networking/Tests/Networking.Simulator.Boiler.Unit/Model/BoilersSetUnitTest.cs
+++ Networking/Tests/Networking.Simulator.Boiler.Unit/Model/BoilersSetUnitTest.cs
@@ -6,7 +6,12 @@
 //___________________________________________________________________________________
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UAOOI.Networking.Simulator.Boiler.AddressSpace;
 using UAOOI.Networking.Simulator.Boiler.Model;
+using UAOOI.SemanticData.UANodeSetValidation.DataSerialization;
+using Boilers = Commsvr.UA.Examples.BoilersSet;
 
 namespace UAOOI.Networking.Simulator.Boiler.UnitTest.Model
 {
@@ -16,8 +21,32 @@ namespace UAOOI.Networking.Simulator.Boiler.UnitTest.Model
     [TestMethod]
     public void ConstructorTest()
     {
-      BoilersSet _set = BoilersSet.Factory;
-      Assert.IsNotNull(_set);
+      using (BoilersSet _set = BoilersSet.Factory)
+      {
+        Assert.IsNotNull(_set);
+        Assert.AreEqual<string>(Boilers.BrowseNames.BoilersArea, _set.BrowseName.Name);
+        Assert.AreEqual<NodeClass>(NodeClass.Object_1, _set.NodeClass);
+        Assert.IsNull(_set.Parent);
+      }
     }
+    [TestMethod]
+    public void GetSemanticDataSourcesTest()
+    {
+      using (ISemanticDataSource _set = BoilersSet.Factory)
+      {
+        Assert.IsNotNull(_set);
+        _set.GetSemanticDataSources((semanticDataSetRootBrowsePath, variableRelativeBrowsePath, variable) => AddressSpace.Add($"{semanticDataSetRootBrowsePath}:{variableRelativeBrowsePath}", variable));
+        Assert.AreEqual<int>(80, AddressSpace.Count);
+        foreach (KeyValuePair<string, IVariable> _var in AddressSpace)
+          Debug.WriteLine($"{_var.Key}:{_var.Value.ValueType}");
+
+      }
+      Assert.IsTrue(AddressSpace.ContainsKey("Boiler #1:CCX001_ControlOut"));
+      Assert.IsTrue(AddressSpace.ContainsKey("Boiler #2:CCX001_ControlOut"));
+      Assert.IsTrue(AddressSpace.ContainsKey("Boiler #3:CCX001_ControlOut"));
+      Assert.IsTrue(AddressSpace.ContainsKey("Boiler #4:CCX001_ControlOut"));
+    }
+    private Dictionary<string, IVariable> AddressSpace = new Dictionary<string, IVariable>();
+
   }
 }

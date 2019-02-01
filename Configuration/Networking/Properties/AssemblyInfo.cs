8c72085b6910edd31481e3521037fcc465ad7243
mpostol <mpostol@cas.eu>
2019-02-01 10:21:13 +0100

    Create Nuget packages #272
    - fixed not relevant compilation errors in the `Release` solution configuration
    - UT :+1:

---
+++ Configuration/Networking/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿//___________________________________________________________________________________
+//
+//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTrademark("Object Oriented Internet")]
+[assembly: Guid("a5863757-f1fc-4f49-a984-a7d17ac3fb61")]
+[assembly: InternalsVisibleTo(
+  "UAOOI.Configuration.Networking.UnitTest, PublicKey=" +
+    "00240000048000009400000006020000002400005253413100040000010001005b97a0972ff6b13a" +
+    "8a9ff9c09503aea0e5e2fe29cb2275a0c0942182f4c3431814b6bc9a556d9fe0d7e7823439c1ba28" +
+    "521f6318e4c936c4461604ef668e9686c2021571b093e1bfba071b373bc56a07a3afdc120c5313d3" +
+    "9a935cda64b759f857ebb3db483641444a5347e1564f8ba6d4fad2f968d3caf9991a4fa6aa019ebe"
+)]

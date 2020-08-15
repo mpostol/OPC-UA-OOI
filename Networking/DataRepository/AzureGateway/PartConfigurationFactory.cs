429c477a3d41c914ddf8ef5d41b76265cec2c83a
mpostol <mpostol@cas.eu>
2020-08-15 10:11:48 +0200

    Implement IConfigurationFactory #454
    - implemented following the example UAOOI.Networking.Simulator.Boiler.ProducerConfigurationFactory

--- Networking/DataRepository/AzureGateway/PartConfigurationFactory.cs
+++ Networking/DataRepository/AzureGateway/PartConfigurationFactory.cs
@@ -5,32 +5,67 @@
 //  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
 //___________________________________________________________________________________
 
+using CommonServiceLocator;
 using System;
+using System.Diagnostics;
+using UAOOI.Common.Infrastructure.Diagnostic;
 using UAOOI.Configuration.Networking;
 using UAOOI.Configuration.Networking.Serialization;
 
 namespace UAOOI.Networking.DataRepository.AzureGateway
 {
-  internal class PartConfigurationFactory : IConfigurationFactory
+  /// <summary>
+  /// Class ProducerConfigurationFactory - provides implementation of the <see cref="ConfigurationFactoryBase{T}"/> for the producer.
+  /// </summary>
+  internal class PartConfigurationFactory : ConfigurationFactoryBase<ConfigurationData>
   {
-    private readonly string _configurationFileName;
+    #region constructor
 
-    public PartConfigurationFactory(string configurationFileName)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PartConfigurationFactory" /> class.
+    /// </summary>
+    /// <param name="configurationFileName">Name of the producer configuration file.</param>
+    public PartConfigurationFactory(string configurationFileName) : base(configurationFileName)
     {
+      IServiceLocator _serviceLocator = ServiceLocator.Current;
+      //TODO Create and Register the EventSource #455
+      _TraceSource = _serviceLocator.GetInstance<ITraceSource>();
+      _TraceSource.TraceData(TraceEventType.Information, 36, $"Starting {nameof(PartConfigurationFactory)} with the configuration file name {configurationFileName}");
       _configurationFileName = configurationFileName;
     }
 
-    #region IConfigurationFactory
+    #endregion constructor
 
-    public event EventHandler<EventArgs> OnAssociationConfigurationChange;
+    #region ConfigurationFactoryBase
 
-    public event EventHandler<EventArgs> OnMessageHandlerConfigurationChange;
+    /// <summary>
+    /// Occurs after the association configuration has been changed.
+    /// </summary>
+    public override event EventHandler<EventArgs> OnAssociationConfigurationChange;
 
-    public ConfigurationData GetConfiguration()
+    /// <summary>
+    /// Occurs after the communication configuration has been changed.
+    /// </summary>
+    public override event EventHandler<EventArgs> OnMessageHandlerConfigurationChange;
+
+    protected override void TraceData(TraceEventType eventType, int id, object data)
     {
-      throw new NotImplementedException();
+      _TraceSource.TraceData(eventType, id, data);
     }
 
-    #endregion IConfigurationFactory
+    protected override void RaiseEvents()
+    {
+      OnAssociationConfigurationChange?.Invoke(this, EventArgs.Empty);
+      OnMessageHandlerConfigurationChange?.Invoke(this, EventArgs.Empty);
+    }
+
+    #endregion ConfigurationFactoryBase
+
+    #region private
+
+    private readonly string _configurationFileName;
+    private ITraceSource _TraceSource = null;
+
+    #endregion private
   }
 }

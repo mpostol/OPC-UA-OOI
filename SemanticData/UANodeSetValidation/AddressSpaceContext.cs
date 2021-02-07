9184c83059e0722b5b8c7ba2a1c4b55c7fd20513
mpostol <mpostol@cas.eu>
2021-02-07 15:12:12 +0100

    ADI model from Embedded example import fails #509
    - changed version information
    - added anchor for further development - the problem is selection of the correct namespace

--- SemanticData/UANodeSetValidation/AddressSpaceContext.cs
+++ SemanticData/UANodeSetValidation/AddressSpaceContext.cs
@@ -1,6 +1,6 @@
 ﻿//___________________________________________________________________________________
 //
-//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
+//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
 //
 //  To be in touch join the community at GITTER: https://gitter.im/mpostol/OPC-UA-OOI
 //___________________________________________________________________________________
@@ -20,14 +20,13 @@ using UAOOI.SemanticData.UANodeSetValidation.XML;
 
 namespace UAOOI.SemanticData.UANodeSetValidation
 {
-
   /// <summary>
   /// Class AddressSpaceContext - responsible to manage all nodes in the OPC UA Address Space.
   /// </summary>
   internal class AddressSpaceContext : IAddressSpaceContext, IAddressSpaceBuildContext, IAddressSpaceValidationContext
   {
-
     #region constructor
+
     /// <summary>
     /// Initializes a new instance of the <see cref="AddressSpaceContext" /> class.
     /// </summary>
@@ -44,9 +43,11 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       ImportNodeSet(_standard);
       m_TraceEvent(TraceMessage.DiagnosticTraceMessage("Address Space - has bee created successfully."));
     }
-    #endregion
+
+    #endregion constructor
 
     #region IAddressSpaceContext
+
     /// <summary>
     /// Sets the information model factory, which can be used to export a part of the OPC UA Address Space. If not set or set null an internal stub implementation will be used.
     /// </summary>
@@ -63,6 +64,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       }
       private get => m_InformationModelFactory;
     }
+
     /// <summary>
     /// Imports a part of the OPC UA Address Space contained in the <see cref="UANodeSet" /> object model.
     /// </summary>
@@ -75,6 +77,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         throw new ArgumentNullException("model", "the model cannot be null");
       ImportNodeSet(model);
     }
+
     /// <summary>
     /// Imports a part of the OPC UA Address Space contained in the file <see cref="FileInfo" />.
     /// </summary>
@@ -90,6 +93,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       UANodeSet _nodeSet = UANodeSet.ReadModelFile(model);
       ImportNodeSet(_nodeSet);
     }
+
     /// <summary>
     /// Validates and exports the selected model for the default namespace at index 1 if defined or standard OPC UA.
     /// </summary>
@@ -100,6 +104,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       m_TraceEvent(TraceMessage.DiagnosticTraceMessage(string.Format("Entering AddressSpaceContext.ValidateAndExportModel - starting for the {0} namespace.", _namespace)));
       ValidateAndExportModel(_nsi, m_Validator);
     }
+
     /// <summary>
     /// Validates and exports the selected model.
     /// </summary>
@@ -107,15 +112,18 @@ namespace UAOOI.SemanticData.UANodeSetValidation
     /// <exception cref="System.ArgumentOutOfRangeException">targetNamespace;Cannot find this namespace</exception>
     void IAddressSpaceContext.ValidateAndExportModel(string targetNamespace)
     {
+      //TODO ADI model from Embedded example import fails #509
       m_TraceEvent(TraceMessage.DiagnosticTraceMessage(string.Format("Entering IAddressSpaceContext.ValidateAndExportModel - starting for the {0} namespace.", targetNamespace)));
       int _nsIndex = m_NamespaceTable.GetIndex(targetNamespace);
       if (_nsIndex == -1)
         throw new ArgumentOutOfRangeException("targetNamespace", "Cannot find this namespace");
       ValidateAndExportModel(_nsIndex, m_Validator);
     }
-    #endregion
+
+    #endregion IAddressSpaceContext
 
     #region IAddressSpaceBuildContext
+
     /// <summary>
     /// Search the address space to find the node <paramref name="nodeId" /> and returns <see cref="XmlQualifiedName" />
     /// encapsulating the <see cref="UANode.BrowseName" /> of this node if exist. Returns<c>null</c> otherwise.
@@ -132,6 +140,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         return null;
       return _context.ExportNodeBrowseName();
     }
+
     /// <summary>
     /// Exports the argument for a method.
     /// </summary>
@@ -152,6 +161,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         _ret.AddDescription(argument.Description.Locale, argument.Description.Text);
       return _ret;
     }
+
     /// <summary>
     /// Gets the or create node context.
     /// </summary>
@@ -168,6 +178,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       }
       return _ret;
     }
+
     /// <summary>
     /// Gets the index or append.
     /// </summary>
@@ -177,6 +188,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
     {
       return m_NamespaceTable.GetIndexOrAppend(value);
     }
+
     /// <summary>
     /// Gets the namespace.
     /// </summary>
@@ -185,6 +197,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
     {
       return m_NamespaceTable.GetString(namespaceIndex);
     }
+
     /// <summary>
     /// Gets my references from the main collection.
     /// </summary>
@@ -194,6 +207,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
     {
       return m_References.Values.Where<UAReferenceContext>(x => (x.ParentNode == index));
     }
+
     /// <summary>
     /// Gets the references2 me.
     /// </summary>
@@ -203,6 +217,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
     {
       return m_References.Values.Where<UAReferenceContext>(x => x.TargetNode == index && x.ParentNode != index);
     }
+
     /// <summary>
     /// Gets the children nodes for the <paramref name="rootNode" />.
     /// </summary>
@@ -214,6 +229,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
                                                                   Where<UAReferenceContext>(x => (x.ReferenceKind == ReferenceKindEnum.HasProperty || x.ReferenceKind == ReferenceKindEnum.HasComponent)).
                                                                   Select<UAReferenceContext, IUANodeContext>(x => x.TargetNode);
     }
+
     public Parameter ExportArgument(DataSerialization.Argument argument)
     {
       XmlQualifiedName _dataType = ExportBrowseName(NodeId.Parse(argument.DataType.Identifier), DataTypeIds.BaseDataType);
@@ -255,24 +271,31 @@ namespace UAOOI.SemanticData.UANodeSetValidation
     //  }
     //  return _ret;
     //}
-    #endregion    
+    #endregion IAddressSpaceBuildContext
+
+
 
     #region IAddressSpaceValidationContext
+
     /// <summary>
     /// Exports the current namespace table containing all namespaces that have been registered.
     /// </summary>
     /// <value>An instance of <see cref="IEnumerable{IModelTableEntry}" /> containing.</value>
     public IEnumerable<IModelTableEntry> ExportNamespaceTable => m_NamespaceTable.ExportNamespaceTable;
-    #endregion
+
+    #endregion IAddressSpaceValidationContext
 
     #region private
+
     //vars
     private readonly IValidator m_Validator;
+
     private IModelFactory m_InformationModelFactory = new InformationModelFactoryBase();
     private Dictionary<string, UAReferenceContext> m_References = new Dictionary<string, UAReferenceContext>();
     private NamespaceTable m_NamespaceTable = null;
     private Dictionary<string, IUANodeContext> m_NodesDictionary = new Dictionary<string, IUANodeContext>();
     private readonly Action<TraceMessage> m_TraceEvent = BuildErrorsHandling.Log.TraceEvent;
+
     //methods
     private void ImportNodeSet(UANodeSet model)
     {
@@ -286,9 +309,10 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       model.RecalculateNodeIds(_modelContext);
       m_TraceEvent(TraceMessage.DiagnosticTraceMessage("AddressSpaceContext.ImportNodeSet - context for imported model is created and starting import nodes."));
       foreach (UANode _nd in model.Items)
-        this.ImportUANode(_nd, m_TraceEvent);
+        ImportUANode(_nd, m_TraceEvent);
       m_TraceEvent(TraceMessage.DiagnosticTraceMessage(string.Format("Finishing AddressSpaceContext.ImportNodeSet - imported {0} nodes.", model.Items.Length)));
     }
+
     private void ImportUANode(UANode node, Action<TraceMessage> traceEvent)
     {
       try
@@ -307,6 +331,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         m_TraceEvent(TraceMessage.DiagnosticTraceMessage(_msg));
       }
     }
+
     private IUANodeContext TryGetUANodeContext(NodeId nodeId, Action<TraceMessage> traceEvent)
     {
       if (!m_NodesDictionary.TryGetValue(nodeId.ToString(), out IUANodeContext _ret))
@@ -321,6 +346,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       }
       return _ret;
     }
+
     private void GetBaseTypes(IUANodeContext rootNode, List<IUANodeContext> inheritanceChain)
     {
       if (rootNode == null)
@@ -337,6 +363,7 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         GetBaseTypes(_derived.First<IUANodeContext>(), inheritanceChain);
       rootNode.InRecursionChain = false;
     }
+
     private void ValidateAndExportModel(int nameSpaceIndex, IValidator validator)
     {
       IEnumerable<IUANodeContext> _stubs = from _key in m_NodesDictionary.Values where _key.NodeIdContext.NamespaceIndex == nameSpaceIndex select _key;
@@ -378,15 +405,18 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         _msg = string.Format("Finishing Validator.ValidateExportModel - the model contains {0} nodes and {1} errors.", _nc, _errors.Count);
       m_TraceEvent(TraceMessage.DiagnosticTraceMessage(_msg));
     }
-    #endregion
+
+    #endregion private
 
     #region UnitTestd
+
     [System.Diagnostics.Conditional("DEBUG")]
     internal void UTAddressSpaceCheckConsistency(Action<IUANodeContext> returnValue)
     {
       foreach (IUANodeContext _node in m_NodesDictionary.Values.Where<IUANodeBase>(x => x.UANode is null))
         returnValue(_node);
     }
+
     [System.Diagnostics.Conditional("DEBUG")]
     internal void UTReferencesCheckConsistency(Action<IUANodeContext, IUANodeContext, IUANodeContext, IUANodeContext> returnValue)
     {
@@ -394,24 +424,26 @@ namespace UAOOI.SemanticData.UANodeSetValidation
         if (_node.SourceNode is null || _node.ParentNode is null || _node.TargetNode is null || _node.TypeNode is null)
           returnValue(_node?.SourceNode, _node?.ParentNode, _node?.TargetNode, _node?.TargetNode);
     }
+
     [System.Diagnostics.Conditional("DEBUG")]
     internal void UTTryGetUANodeContext(NodeId nodeId, Action<IUANodeContext> returnValue)
     {
       returnValue(TryGetUANodeContext(nodeId, x => { }));
     }
+
     [System.Diagnostics.Conditional("DEBUG")]
     internal void UTGetReferences(NodeId source, Action<UAReferenceContext> returnValue)
     {
       foreach (UAReferenceContext _ref in m_References.Values.Where<UAReferenceContext>(x => (x.SourceNode.NodeIdContext == source)))
         returnValue(_ref);
     }
+
     [System.Diagnostics.Conditional("DEBUG")]
     internal void UTValidateAndExportModel(int nameSpaceIndex, Action<IEnumerable<IUANodeContext>> returnValue)
     {
       returnValue((from _key in m_NodesDictionary.Values where _key.NodeIdContext.NamespaceIndex == nameSpaceIndex select _key));
     }
-    #endregion
 
+    #endregion UnitTestd
   }
-
 }

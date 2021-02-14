c031234dbd789aff851ec3dabf3f5a2873efc35e
mpostol <mpostol@cas.eu>
2021-02-14 21:18:51 +0100

    Generate MachineVision ModelDesign format from NodeSet2 format #465
    - added USNodeSet file OPC 40100-1 - Opc.Ua.MachineVision.NodeSet2.xml from OPCF
    - Added UT against this model

--- SemanticData/Tests/USNodeSetValidationUnitTestProject/XMLModelsProblemsToReportUnitTest.cs
+++ SemanticData/Tests/USNodeSetValidationUnitTestProject/XMLModelsProblemsToReportUnitTest.cs
@@ -80,8 +80,35 @@ namespace UAOOI.SemanticData.UANodeSetValidation
       //Extensions is omitted during the import
       List<TraceMessage> importUANodeSetXML = _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.XML).ToList<TraceMessage>();
       Assert.AreEqual<int>(0, importUANodeSetXML.Count);
-      //Assert.AreEqual<string>("P0-0001010000", importUANodeSetXML[0].BuildError.Identifier);
-      //Assert.IsTrue(importUANodeSetXML[0].Message.Contains("Extensions is omitted during the import"));
+      _trace.Clear();
+      _as.ValidateAndExportModel(model);
+      foreach (TraceMessage item in _trace)
+        Debug.WriteLine(item.ToString());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.DataEncoding).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.DataType).Count<TraceMessage>());
+      Assert.AreEqual<int>(3, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.Diagnostic).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.Naming).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.NodeClass).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.NonCategorized).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.Reference).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.XML).Count<TraceMessage>());
+    }
+    [TestMethod]
+    public void MachineVisionTest()
+    {
+      FileInfo _testDataFileInfo = new FileInfo(@"ProblemsToReport\MachineVision\Opc.Ua.MachineVision.NodeSet2.xml");
+      Assert.IsTrue(_testDataFileInfo.Exists);
+      List<TraceMessage> _trace = new List<TraceMessage>();
+      IAddressSpaceContext _as = new AddressSpaceContext(z => _trace.Add(z));
+      Uri model = _as.ImportUANodeSet(_testDataFileInfo);
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.DataEncoding).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.DataType).Count<TraceMessage>());
+      Assert.AreEqual<int>(10, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.Diagnostic).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.Naming).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.NodeClass).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.NonCategorized).Count<TraceMessage>());
+      Assert.AreEqual<int>(0, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.Reference).Count<TraceMessage>());
+      Assert.AreEqual<int>(1, _trace.Where<TraceMessage>(x => x.BuildError.Focus == Focus.XML).Count<TraceMessage>());
       _trace.Clear();
       _as.ValidateAndExportModel(model);
       foreach (TraceMessage item in _trace)

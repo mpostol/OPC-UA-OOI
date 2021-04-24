a5712e972a577aaa4bf0b8575c6150c9b470e168
Mariusz <mpostol@cas.eu>
2021-04-24 20:44:54 +0200

    Import all dependencies for the model #575
    - implemented Load main functionality - must be integrated with the Discovery library
    - UT :+1:

---
+++ SemanticData/UANodeSetValidation/XML/Discovery.cs
@@ -0,0 +1,24 @@
+﻿//___________________________________________________________________________________
+//
+//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GITTER: https://github.com/mpostol/OPC-UA-OOI
+//___________________________________________________________________________________
+
+using System;
+
+namespace UAOOI.SemanticData.UANodeSetValidation.XML
+{
+  //TODO Import all dependencies for the model #575
+  internal class Discovery
+  {
+    internal static Discovery Instance => _Instance.Value;
+
+    internal UANodeSet LoadUANodeSet(string modelUri)
+    {
+      return null;
+    }
+
+    private static Lazy<Discovery> _Instance = new Lazy<Discovery>(() => new Discovery());
+  }
+}

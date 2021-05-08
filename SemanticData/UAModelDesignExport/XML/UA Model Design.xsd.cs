17aa24f90458c3a9cad0bbcd4ab5010b43b5d6ab
Mariusz <mpostol@cas.eu>
2021-05-08 23:20:25 +0200

    Release new version 6.3.0-Delta #598
    - created new version
    - removed *.xsd.cs from .gitignore
    - recovered missed file.

---
+++ SemanticData/UAModelDesignExport/XML/UA Model Design.xsd.cs
@@ -0,0 +1,28 @@
+﻿//__________________________________________________________________________________________________
+//
+//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GitHub: https://github.com/mpostol/OPC-UA-OOI/discussions
+//__________________________________________________________________________________________________
+
+using System.Collections.Generic;
+using System.Xml;
+using UAOOI.Common.Infrastructure.Serializers;
+
+namespace UAOOI.SemanticData.UAModelDesignExport.XML
+{
+  partial class ModelDesign : INamespaces
+  {
+    /// <summary>
+    /// Gets the namespaces that is to be used to parametrize XML document.
+    /// </summary>
+    /// <returns>An instance of IEnumerable[XmlQualifiedName] containing the XML namespaces and prefixes that a serializer uses to generate qualified names in an XML-document instance.</returns>
+    public IEnumerable<XmlQualifiedName> GetNamespaces()
+    {
+      List<XmlQualifiedName> ret = new List<XmlQualifiedName>();
+      foreach (Namespace item in Namespaces)
+        ret.Add(new XmlQualifiedName(item.Prefix, item.XmlNamespace));
+      return ret;
+    }
+  }
+}

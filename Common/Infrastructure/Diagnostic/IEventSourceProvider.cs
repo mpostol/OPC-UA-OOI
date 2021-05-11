7d8a729f9e5f97f8e5440537e1015e99e589a881
Mariusz <mpostol@cas.eu>
2021-05-11 15:24:12 +0200

    Enhance/Improve the Program logging and tracing infrastructure. #590
    - added IEventSourceProvider.cs

---
+++ Common/Infrastructure/Diagnostic/IEventSourceProvider.cs
@@ -0,0 +1,23 @@
+﻿//__________________________________________________________________________________________________
+//
+//  Copyright (C) 2021, Mariusz Postol LODZ POLAND.
+//
+//  To be in touch join the community at GitHub: https://github.com/mpostol/OPC-UA-OOI/discussions
+//__________________________________________________________________________________________________
+
+using System.Diagnostics.Tracing;
+
+namespace UAOOI.Common.Infrastructure.Diagnostic
+{
+  /// <summary>
+  /// Interface IEventSourceProvider - if implemented returns an instance of <see cref="EventSource"/> to be registered by the logging infrastructure.
+  /// </summary>
+  public interface INetworkingEventSourceProvider
+  {
+    /// <summary>
+    /// Gets the part event source.
+    /// </summary>
+    /// <returns>Returns an instance of <see cref="EventSource"/>.</returns>
+    EventSource GetPartEventSource();
+  }
+}

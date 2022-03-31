ec1279c690088d60cbb84c1e3e5b353da005769b
MPostol <mpostol@cas.eu>
2022-04-01 00:03:58 +0200

    Update models to harmonize with current UA-ModelCompiler release #655
    Updated SemanticData/Tests/USNodeSetValidationUnitTestProject/ using UA-Compiler Version 2.1.1.0

---
+++ SemanticData/Tests/USNodeSetValidationUnitTestProject/XMLModels/CorrectModels/GoNodeSet.cmd
@@ -0,0 +1,6 @@
+set COMPILER=OOI.ModelCompilerUI
+
+"%1\%COMPILER%" -d2 "ReferenceTest.xml"  -cg "ReferenceTest.csv" -o "ReferenceTest" -console
+"%1\%COMPILER%" -d2 "ObjectTypeTest.xml"  -cg "ObjectTypeTest.csv" -o "ObjectTypeTest" -console
+"%1\%COMPILER%" -d2 "VariableTypeTest.xml"  -cg "VariableTypeTest.csv" -o "VariableTypeTest" -console
+"%1\%COMPILER%" -d2 "DataTypeTest.xml"  -cg "DataTypeTest.csv" -o "DataTypeTest" -console

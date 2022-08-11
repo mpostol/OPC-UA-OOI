a7ae0fe724a33f3cbbd8928ac801a84b54c52f1a
MPostol <mpostol@cas.eu>
2022-08-11 17:09:25 +0200

    UANodeSetValidation - Define independent Address Space API #672
    - Fixed IUAType

--- SemanticData/UANodeSetValidation/AddressSpace.Abstractions/IUAType.cs
+++ SemanticData/UANodeSetValidation/AddressSpace.Abstractions/IUAType.cs
@@ -15,11 +15,11 @@ namespace UAOOI.SemanticData.AddressSpace.Abstractions
     /// <summary>
     /// Sets a value indicating whether this instance is abstract.
     /// A boolean Attribute with the following values:
-    ///   TRUE it is an abstract DataType.
-    ///   FALSE it is not an abstract DataType.
+    ///   TRUE it is an abstract type, i.e. no instances of this type shall exist, only of its subtypes.
+    ///   FALSE it is not an abstract type, i.e. instances of this type can exist in the Address Space.
     /// </summary>
     /// <remarks>Default Value is false</remarks>
-    /// <value><c>true</c> if this instance is abstract; otherwise, <c>false</c>.</value>
+    /// <value><c>true</c> if this type is abstract; otherwise, <c>false</c>.</value>
     bool IsAbstract { get; set; }
   }
 }

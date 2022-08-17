2af4ebd4f4725ad5794c28318991fb8a4cd9e52a
MPostol <mpostol@cas.eu>
2022-08-17 23:04:44 +0200

    UANodeSetValidation - Define independent Address Space API #672
    - fixed IUAMethod

--- SemanticData/UANodeSetValidation/AddressSpace.Abstractions/IUAMethod.cs
+++ SemanticData/UANodeSetValidation/AddressSpace.Abstractions/IUAMethod.cs
@@ -19,7 +19,8 @@ namespace UAOOI.SemanticData.AddressSpace.Abstractions
   public interface IUAMethod : IUAInstance
   {
     /// <summary>
-    /// Sets a value indicating whether the Method node is executable (“False” means not executable, “True” means executable), not taking user access rights into account.
+    /// Sets a value indicating whether the Method node is executable (“False” means not executable, “True” means executable).The Executable does not take any user access rights into account, 
+    /// i.e. although the Method is executable this may be restricted to a certain user/user group.
     /// If the server cannot get the executable information from the underlying system, it should state that it is executable. If a Method is called, the server should transfer
     /// this request and return the corresponding StatusCode if such a request is rejected.
     /// </summary>
@@ -37,12 +38,26 @@ namespace UAOOI.SemanticData.AddressSpace.Abstractions
     /// It is the NodeId of the UAMethod with the same BrowseName contained in the TypeDefinition associated with the Object Node.
     /// If the TypeDefinition overrides a Method inherited from a base ObjectType then this attribute shall reference the Method Node in the subtype.
     /// </summary>
+    /// <remarks>
+    /// It is not exposed in the Address Space
+    /// </remarks>
     /// <value>The method declaration identifier.</value>
     string MethodDeclarationId { set; get; }
-
+    /// <summary>
+    /// The property is used to specify the arguments that shall be used by a client when calling the Method.
+    /// </summary>
+    /// <remarks>
+    /// It is not exposed in the Address Space    
+    /// </remarks>
     UAMethodArgument[] ArgumentDescription { get; set; }
   }
 
+  /// <summary>
+  /// This UAMethodArgument defines a method input or output argument specification. It is for example used in the input and output argument Properties for Methods. 
+  /// </summary>
+  /// <remarks>
+  /// It is standard type defined in P3 8.6 Argument
+  /// </remarks>
   public class UAMethodArgument
   {
     /// <remarks/>
